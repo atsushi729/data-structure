6b8c45d7561786560a89aeb22e416ee5b8667b11
Atsushi <atu729@icloud.com>
2023-03-25 23:09:17 +0900

    add 301 different version

--- Python/301-valid-parentheses.py
+++ Python/301-valid-parentheses.py
@@ -19,3 +19,21 @@ def isValid(paren) -> bool:
 
 paren = list(input())
 print(isValid(paren))
+
+# different version
+def is_valid(s):
+    stack = []
+    for char in s:
+        if char in ['(', '{', '[']:
+            stack.append(char)
+        elif char in [')', '}', ']']:
+            if not stack:
+                return False
+            if char == ')' and stack[-1] != '(':
+                return False
+            elif char == '}' and stack[-1] != '{':
+                return False
+            elif char == ']' and stack[-1] != '[':
+                return False
+            stack.pop()
+    return not stack
