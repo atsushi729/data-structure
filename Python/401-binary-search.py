83c72831200e6243645480ad7b3b1147d4af0906
Atsushi <atu729@icloud.com>
2023-03-28 23:47:46 +0900

    update 401 practice code

--- Python/401-binary-search.py
+++ Python/401-binary-search.py
@@ -1,19 +1,31 @@
 def search(nums: list[int], target: int):
-    numbers = sorted(nums)
 
-    middle = round(len(numbers) / 2)
-    middle_value = numbers[middle]
+    while len(nums) >= 1:
+        middle = round(len(nums) / 2) - 1
+        middle_value = nums[middle]
 
-    if target == middle_value:
-        return middle
-    elif target > middle_value:
-        numbers[0:middle] = []
-    else:
-        numbers[middle:-1] = []
-
-    return numbers
+        if target == middle_value:
+            return middle
+        elif target > middle_value:
+            nums[0:middle + 1] = []
+        elif target < middle_value:
+            nums[middle + 1:-1] = []
+        else:
+            return -1
+    return nums
 
 
 nums = [int(x) for x in input().split()]
 target = int(input())
 print(search(nums, target))
+
+
+####################
+# Simple solutions #
+####################
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         if target in nums:
+#             return nums.index(target)
+#         else:
+#             return -1
