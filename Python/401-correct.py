ff7f9f65554ed1dbab2f2d9289061a49ef43aef1
Atsushi <atu729@icloud.com>
2023-03-29 23:39:04 +0900

    update 401 model answer

---
+++ Python/401-correct.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: list[int], target: int):
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = l + ((r - l) // 2)
+
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return
+        return -1
