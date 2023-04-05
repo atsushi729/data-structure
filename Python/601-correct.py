0296f494297cb9f7f9a7ef8dfc0462117f8482ab
Atsushi <atu729@icloud.com>
2023-04-05 23:54:53 +0900

    601 add another solution

--- Python/601-correct.py
+++ Python/601-correct.py
@@ -10,3 +10,29 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
+
+## Another solution
+# from collections import deque
+#
+#
+# def invertTree(root: TreeNode) -> TreeNode:
+#     if not root:
+#         return None
+#
+#     queue = deque([root])
+#
+#     while queue:
+#         node = queue.popleft()
+#
+#         # Swap the left and right children of the current node
+#         temp = node.left
+#         node.left = node.right
+#         node.right = temp
+#
+#         # Add the left and right children to the queue if they exist
+#         if node.left:
+#             queue.append(node.left)
+#         if node.right:
+#             queue.append(node.right)
+#
+#     return root
