36aa1f77b987c45375e6914877ab5a3343c86619
Atsushi <atu729@icloud.com>
2023-04-08 22:42:17 +0900

    602 add model solution

--- Python/602-correct.py
+++ Python/602-correct.py
@@ -4,3 +4,22 @@ class Solution:
             return 0
 
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+# Iterative DFS
+class Solutions:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        stack = [[root, 1]]
+        result = 1
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+
+        return result
