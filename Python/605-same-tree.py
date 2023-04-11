f997ec2d96b1db78578272da4fa0da366c28e097
Atsushi <atu729@icloud.com>
2023-04-11 22:37:11 +0900

    605 add another solution

--- Python/605-same-tree.py
+++ Python/605-same-tree.py
@@ -31,3 +31,16 @@ class Solution:
 
 bt = Solution(root)
 print(bt.isSameTree(root, root))
+
+## another solution
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
