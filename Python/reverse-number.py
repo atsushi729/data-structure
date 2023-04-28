e9331173e832a2657dd230a6e87375012b53c031
Atsushi <atu729@icloud.com>
2023-04-28 23:51:29 +0900

    add another solution

--- Python/reverse-number.py
+++ Python/reverse-number.py
@@ -9,6 +9,13 @@ class Solution:
 
         return reversed_num
 
+    ## Sometimes, it is not allow to use type of string.
+    def anotherReverseNumber(self, num: int) -> int:
+        num_str = str(num)
+        reversed_str = num_str[::-1]
+        reversed_num = int(reversed_str)
+        return reversed_num
+
 
 if __name__ == '__main__':
     sol = Solution()
