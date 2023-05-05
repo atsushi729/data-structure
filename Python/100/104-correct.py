65057f74254df7a78fb9f06851e8a7f7829b3d92
Atsushi <atu729@icloud.com>
2023-05-05 18:16:51 +0900

    add another solution

--- Python/100/104-correct.py
+++ Python/100/104-correct.py
@@ -14,3 +14,20 @@ class Solution:
             if (r - l) * h <= res:
                 break
         return res
+
+    ## Another solution
+    def maxArea(self, height: list[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+
+        while left < right:
+            current_area = min(height[left], height[right]) * (right - left)
+            max_area = max(max_area, current_area)
+
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
