6930dedf27b180f2067c175da6354d4db0c88ddc
Atsushi <atu729@icloud.com>
2023-05-06 23:57:37 +0900

    add another solution

--- Python/000/001-contain-duplicate.py
+++ Python/000/001-contain-duplicate.py
@@ -1,5 +1,5 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+    def containsDuplicate(self, nums: list[int]) -> bool:
         if len(nums) != len(set(nums)):
             return True
         else:
@@ -14,3 +14,15 @@ def containsDuplicate(nums):
             return True
         seen.add(num)
     return False
+
+
+## another solution
+def containsDuplicate(nums):
+    sortedNum = sorted(nums)
+    for i in range(len(sortedNum) - 1):
+        if sortedNum[i] == sortedNum[i + 1]:
+            return True
+    return False
+
+
+print(containsDuplicate([1, 2, 3]))
