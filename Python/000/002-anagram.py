8926b22e70c3b226611615c3f0d1c7a95114f02f
Atsushi <atu729@icloud.com>
2023-05-07 22:06:37 +0900

    add another solution

--- Python/000/002-anagram.py
+++ Python/000/002-anagram.py
@@ -7,3 +7,33 @@ class Solution:
             return True
         else:
             return False
+
+    ## another solution
+    def isAnagram2(self, y, t):
+        # Check if the two strings have the same length
+        if len(y) != len(t):
+            return False
+
+        # Create a frequency table for string s
+        freq = {}
+        for char in y:
+            if char in freq:
+                freq[char] += 1
+            else:
+                freq[char] = 1
+
+        # Check if the characters in string t are in the frequency table
+        for char in t:
+            if char in freq and freq[char] > 0:
+                freq[char] -= 1
+            else:
+                return False
+
+        return True
+
+
+if __name__ == '__main__':
+    y = "anagram"
+    t = "nagaram"
+    s = Solution()
+    print(s.isAnagram2(y, t))
