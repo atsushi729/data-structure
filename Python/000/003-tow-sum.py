5ca5f0a84d7993259a39845817553576be959c05
Atsushi <atu729@icloud.com>
2023-05-08 22:39:42 +0900

    add another solution

--- Python/000/003-tow-sum.py
+++ Python/000/003-tow-sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         prevMap = {}  # val -> index
 
         for i, n in enumerate(nums):
@@ -7,3 +7,13 @@ class Solution:
             if diff in prevMap:
                 return [prevMap[diff], i]
             prevMap[n] = i
+
+    ## Another solution
+    class Solution:
+        def twoSum(self, nums: list[int], target: int) -> list[int]:
+            for i in range(len(nums)):
+                rest = target - nums[i]
+                for j in range(i + 1, len(nums)):
+                    if nums[j] == rest:
+                        output = [i, j]
+            return output
