d7a34ef4f6c7168a4241ea6311ac4c07da50d68c
Atsushi <atu729@icloud.com>
2023-05-10 22:56:33 +0900

    add progress file

--- Python/200/202-Longest-Substring-Without-Repeating-Characters.py
+++ Python/200/202-Longest-Substring-Without-Repeating-Characters.py
@@ -4,19 +4,25 @@ class Solution:
             return 0
 
         baseCharacter = "abcdefghijklmnopqrstuvwxyz"
-        counter = 0
-        tmp = 0
+        counter = 1
+        tmp = 1
+        increment = 1
 
         for i, word in enumerate(s):
-            while s[i + i] == baseCharacter[i + 1]:
-                tmp += 1
+            position = s.index(word)
+            if i + increment < len(s):
+                while s[i + increment] == baseCharacter[position + increment]:
+                    tmp += 1
+                    increment += 1
+                    if i + increment >= len(s):
+                        break
 
-            counter = max(counter, tmp)
-            tmp = 0
+                counter = max(counter, tmp)
+                tmp = increment = 1
 
         return counter
 
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.lengthOfLongestSubstring("abcabcbb"))
+    print(s.lengthOfLongestSubstring("pwwkew"))
