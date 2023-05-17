ca04082dffec46fd25944a08453ebf19c3376cb1
Atsushi <atu729@icloud.com>
2023-05-17 23:59:15 +0900

    fixed arithmetic part

--- Python/200/204-correct.py
+++ Python/200/204-correct.py
@@ -12,7 +12,7 @@ class Solution:
         for i in range(26):
             matches += 1 if s1Count[i] == s2Count[i] else 0
 
-        l =0
+        l = 0
         for r in range(len(s1), len(s2)):
             if matches == 26:
                 return True
@@ -28,7 +28,7 @@ class Solution:
             s2Count[index] -= 1
             if s1Count[index] == s2Count[index]:
                 matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
+            elif s1Count[index] - 1 == s2Count[index]:
                 matches -= 1
             l += 1
         return matches == 26
