cc3733bad34d9fdf13bb834a58463cf433fb2371
Atsushi <atu729@icloud.com>
2023-06-15 23:53:35 +0900

    add new solution

--- Python/other/most-frequent-string.py
+++ Python/other/most-frequent-string.py
@@ -1,8 +1,9 @@
-from typing import Tuple, List
+import operator
+from typing import Tuple
 
 
 class Solution:
-    def count_string(self, text: str) -> tuple[list[str], int]:
+    def count_string(self, text: str) -> Tuple[list[str], int]:
         text = text.lower().replace(" ", "")
         counter = {}
 
@@ -17,8 +18,17 @@ class Solution:
 
         return max_object, max(counter.values())
 
+    def count_string_new(self, text: str) -> tuple[str, int]:
+        text = text.lower()
+        container = []
+        for word in text:
+            if not word.isspace():
+                container.append((word, text.count(word)))
+
+        return max(container, key=operator.itemgetter(1))
+
 
 if __name__ == "__main__":
     solution = Solution()
     text = "this is my dream"
-    print(solution.count_string(text))
+    print(solution.count_string_new(text))
