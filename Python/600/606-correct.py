50074eac9216f9a42620e3d1f2922cb29a3777a8
Atsushi <atu729@icloud.com>
2023-06-22 23:53:35 +0900

    add another solution

--- Python/600/606-correct.py
+++ Python/600/606-correct.py
@@ -21,3 +21,18 @@ class Solution:
         if s and t and s.val == t.val:
             return self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right)
         return False
+
+
+## Version 2
+class Solution:
+    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+        if not s:
+            return False
+        if self.isSameTree(s, t):
+            return True
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q
