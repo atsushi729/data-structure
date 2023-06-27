89884f8927ab8b1ae2043d4eada8b7ef015cacc3
Atsushi <atu729@icloud.com>
2023-06-27 23:49:05 +0900

    add solution

--- Python/other/order-even-first-odd-last.py
+++ Python/other/order-even-first-odd-last.py
@@ -10,7 +10,17 @@ def order_even_first_odd_last(numbers: list[int]) -> list[int]:
     numbers[:] = even_list + odd_list
     return numbers
 
+def v2(numbers: list[int]) -> list[int]:
+    i, j = 0, len(numbers) - 1
+
+    while i < j:
+        if numbers[i] % 2 == 0:
+            i += 1
+        else:
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+            j -= i
+    return numbers
 
 if __name__ == "__main__":
     numbers = [0, 1, 4, 6, 3, 2, 5, 6, 3, 6, 7, 9, 4, ]
-    print(order_even_first_odd_last(numbers))
+    print(v2(numbers))
