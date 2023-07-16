d96b110556db15634690e26fde6b75883b5b07a0
Atsushi <atu729@icloud.com>
2023-07-16 23:55:08 +0900

    add version 2 solution

--- Python/other/202-Happy-Number-correct.py
+++ Python/other/202-Happy-Number-correct.py
@@ -18,6 +18,31 @@ class Solution:
         return output
 
 
+"""
+Version 2
+"""
+class SolutionV2:
+    def isHappy(self, n: int) -> bool:
+        visit = set()
+
+        while n not in visit:
+            visit.add(n)
+            n = self.sumSquareDigits(n)
+
+            if n == 1:
+                return True
+
+        return False
+
+    def sumSquareDigits(self, n):
+        output = 0
+
+        while n:
+            output += (n % 10) ** 2
+            n = n // 10
+        return output
+
+
 if __name__ == "__main__":
     solution = Solution()
     n = 19
