55b1947cbfb90f96ea83b083e5016f9c1f19a882
Atsushi <atu729@icloud.com>
2023-08-02 23:50:24 +0900

    add other solution

--- Python/other/739-Daily-Temperatures-solved.py
+++ Python/other/739-Daily-Temperatures-solved.py
@@ -12,3 +12,27 @@ class Solution:
             stack.append(i)
 
         return answer
+
+    def dailyTemperatures_v2(self, temperatures: list[int]) -> list[int]:
+        stack = []
+        size = len(temperatures)
+        ans = [0] * size
+
+        for i in range(size):
+            if not stack or temperatures[i] <= stack[-1][0]:
+                stack.append((temperatures[i], i))
+                continue
+
+            while stack and temperatures[i] > stack[-1][0]:
+                temp, idx = stack.pop()
+                ans[idx] = abs(i - idx)
+
+            stack.append((temperatures[i], i))
+
+        return ans
+
+
+if __name__ == "__main__":
+    s = Solution()
+    temperatures = [89, 62, 70, 58, 47, 47, 46, 76, 100, 70]
+    print(s.dailyTemperatures_v2(temperatures))
