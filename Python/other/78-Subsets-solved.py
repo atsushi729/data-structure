6951502494c7e56ff58f4734935d512c8babb5e3
Atsushi <atu729@icloud.com>
2023-08-08 00:27:36 +0900

    add another solution

---
+++ Python/other/78-Subsets-solved.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        res = []
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [0]
+    print(s.subsets(nums))
