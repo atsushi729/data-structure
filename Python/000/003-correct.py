61c107ff213587cd00ff4e6649e65a49dc95c341
Atsushi <atu729@icloud.com>
2023-08-10 22:45:22 +0900

    add brute force solution

--- Python/000/003-correct.py
+++ Python/000/003-correct.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         prevMap = {}  # val -> index
 
         for i, n in enumerate(nums):
@@ -7,3 +7,16 @@ class Solution:
             if diff in prevMap:
                 return [prevMap[diff], i]
             prevMap[n] = i
+
+    def twoSumBruteForce(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [3, 2, 4]
+    target = 6
+    print(s.twoSumBruteForce(nums, target))
