5883695bec96ae4bb5ffbff43cf421c08b6e3b89
Atsushi <atu729@icloud.com>
2023-08-12 11:13:38 +0900

    add explanation

--- Python/000/001-correct.py
+++ Python/000/001-correct.py
@@ -13,3 +13,9 @@ if __name__ == "__main__":
     nums = [1, 2, 3, 4, 4]
     s = Solution()
     print(s.containsDuplicate(nums))
+
+"""
+Why do I use a hash map instead of an array? 
+The reason is that the time complexity of a hash map is always O(1), making it more efficient compared to an array. 
+Using an array would lead to a runtime error due to its time complexity of O(n), as it needs to go through all the elements in the array.
+"""
