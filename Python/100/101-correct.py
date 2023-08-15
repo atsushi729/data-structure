705bb4138112c6d7b0db34924679f6faa8253f1b
Atsushi <atu729@icloud.com>
2023-08-15 23:42:54 +0900

    add new method

--- Python/100/101-correct.py
+++ Python/100/101-correct.py
@@ -21,8 +21,28 @@ class Solution:
             right -= 1
         return True
 
+    def isPalindromeV3(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphanum(s[l]):
+                l += 1
+            while l < r and not self.alphanum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+    def alphanum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        )
+
 
 if __name__ == "__main__":
     st = "A man, a plan, a canal: Panama"
     s = Solution()
-    print(s.isPalindrome(st))
+    print(s.isPalindromeV3(st))
