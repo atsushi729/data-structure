44fd1023209452012b8a4f78cb229f866a173361
Atsushi <atu729@icloud.com>
2023-08-21 21:52:28 +0900

    add solution

---
+++ Python/other/392. Is Subsequence-solved.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s_index = 0
+
+        for char in t:
+            if s_index < len(s) and char == s[s_index]:
+                s_index += 1
+
+        return s_index == len(s)
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    s = "axc"
+    t = "ahbgdc"
+    print(solution.isSubsequence(s, t))
