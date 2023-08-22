dd2a4c45a4904cf1fe752e05f21877fa6dc444c7
Atsushi <atu729@icloud.com>
2023-08-22 21:43:03 +0900

    add solution

--- Python/other/392. Is Subsequence-solved.py
+++ Python/other/392. Is Subsequence-solved.py
@@ -8,9 +8,18 @@ class Solution:
 
         return s_index == len(s)
 
+    def isSubsequenceV2(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == len(s)
+
 
 if __name__ == "__main__":
     solution = Solution()
     s = "axc"
     t = "ahbgdc"
-    print(solution.isSubsequence(s, t))
+    print(solution.isSubsequenceV2(s, t))
