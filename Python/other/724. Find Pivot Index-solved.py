ca7978325329d4dd8fefdec9d7a8d1ecdea0b135
Atsushi <atu729@icloud.com>
2023-09-05 23:11:09 +0900

    add another solution

--- Python/other/724. Find Pivot Index-solved.py
+++ Python/other/724. Find Pivot Index-solved.py
@@ -1,8 +1,8 @@
 class Solution:
     def pivotIndex(self, nums: list[int]) -> int:
         total = sum(nums)  # O(n)
-
         leftSum = 0
+
         for i in range(len(nums)):
             rightSum = total - nums[i] - leftSum
             if leftSum == rightSum:
@@ -10,8 +10,22 @@ class Solution:
             leftSum += nums[i]
         return -1
 
+    def pivotIndexV2(self, nums: list[int]) -> int:
+        left_sum = 0
+        right_sum = sum(nums)
+
+        for i, num in enumerate(nums):
+            right_sum -= num
+
+            if left_sum == right_sum:
+                return i
+
+            left_sum += num
+
+        return -1
+
 
 if __name__ == "__main__":
     s = Solution()
     nums = [1, 7, 3, 6, 5, 6]
-    print(s.pivotIndex(nums))
+    print(s.pivotIndexV2(nums))
