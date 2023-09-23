24cfe572f7570752f3f27e42c862fa3f5fb24c7d
Atsushi <atu729@icloud.com>
2023-09-23 20:45:19 +0900

    add another solution

--- Python/other/2215. Find the Difference of Two Arrays-solved.py
+++ Python/other/2215. Find the Difference of Two Arrays-solved.py
@@ -12,3 +12,15 @@ class Solution:
                 out[1].append(i)
 
         return out
+
+    def findDifferenceV2(self, nums1: list[int], nums2: list[int]) -> list[list[int]]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return [set1.difference(set2), set2.difference(set1)]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums1 = [1, 2, 3]
+    nums2 = [2, 4, 6]
+    print(s.findDifferenceV2(nums1, nums2))
