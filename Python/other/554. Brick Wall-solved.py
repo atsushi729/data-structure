96ba9548e3db1cbc80dd93963f1c5c58e0a24dc8
Atsushi <atu729@icloud.com>
2023-09-29 23:40:54 +0900

    add comment solution

--- Python/other/554. Brick Wall-solved.py
+++ Python/other/554. Brick Wall-solved.py
@@ -1,11 +1,11 @@
 class Solution:
     def leastBricks(self, wall: list[list[int]]) -> int:
-        countGap = {0: 0}
+        countGap = {0: 0}  # create hash map which store space information.
 
         for r in wall:
             total = 0
             for b in r[:-1]:
-                total += b
+                total += b  # add total which can be used as key for hash map
                 countGap[total] = 1 + countGap.get(total, 0)
 
         return len(wall) - max(countGap.values())
