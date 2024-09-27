1b5b8403f355f337c885041b4205009943e2600b
Atsushi <atu729@icloud.com>
2024-09-27 12:30:06 +0900

    add top k frequent element algorithm

---
+++ Python/300/347-top-k-frequent-elements.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+#################### Solution ####################
+def top_k_frequent(nums: [int], k: int) -> [int]:
+    frequent_counter = {}
+
+    # Map object to count frequency of nums.
+    # e.g. {1 : 1, 2 : 2, 3 : 3}
+    for n in nums:
+        if n in frequent_counter:
+            continue
+        count = nums.count(n)
+        frequent_counter[n] = count
+
+    # Sorted based on value.
+    # [(3, 3), (2, 2), (1, 1)]
+    sorted_frequent_counter = sorted(frequent_counter.items(), key=lambda x: x[1], reverse=True)
+
+    # Create key  which contain top k frequents value.
+    top_k_frequents = [item[0] for item in sorted_frequent_counter[:k]]
+
+    return top_k_frequents
+
+
+#################### Test Case ####################
+class TestTopKFrequent(unittest.TestCase):
+    def test_top_k_frequent(self):
+        self.assertEqual(top_k_frequent([1, 1, 1, 2, 2, 3], 2), [1, 2])
+
+
+#################### Test via terminal ####################
+nums = [1, 1, 1, 2, 2, 3]
+k = 2
+print(top_k_frequent(nums, k))  # [1, 2]
