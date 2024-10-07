cdf3127091c3c2a52178e81b35ecb5d00677f0db
Atsushi <atu729@icloud.com>
2024-10-07 19:29:57 +0900

    add model solution

--- Python/000/049-group-anagram.py
+++ Python/000/049-group-anagram.py
@@ -1,4 +1,5 @@
 import unittest
+import collections
 
 
 #################### Solution ####################
@@ -13,8 +14,26 @@ def group_anagram(strs):
     return list(anagrams.values())
 
 
+# Another solution
+def model_group_anagram(strs: list[str]) -> list[list[str]]:
+    ans = collections.defaultdict(list)
+
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord("a")] += 1
+        ans[tuple(count)].append(s)
+    return list(ans.values())
+
+
 #################### Test Case ####################
 class TestGroupAnagram(unittest.TestCase):
     def test_group_anagram(self):
-        assert group_anagram(["eat", "tea", "tan", "ate", "nat", "bat"]) == [['eat', 'tea', 'ate'], ['tan', 'nat'],
-                                                                             ['bat']]
+        self.assertEqual(
+            group_anagram(["eat", "tea", "tan", "ate", "nat", "bat"]), [['eat', 'tea', 'ate'], ['tan', 'nat'],
+                                                                        ['bat']])
+
+    def test_model_group_anagram(self):
+        self.assertEqual(model_group_anagram(["eat", "tea", "tan", "ate", "nat", "bat"]), [['eat', 'tea', 'ate'],
+                                                                                           ['tan', 'nat'],
+                                                                                           ['bat']])
