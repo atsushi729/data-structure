715c9b8af5af63861b37fd199af8efe1d7b246eb
Atsushi <atu729@icloud.com>
2024-10-10 19:22:52 +0900

    add container with most water

---
+++ Python/000/011-container-with-most-water.py
@@ -0,0 +1,27 @@
+#################### Solution ####################
+import unittest
+
+
+def max_area(heights: list[int]) -> int:
+    max_amount = 0
+    left, right = 0, len(heights) - 1
+
+    while left < right:
+        width = right - left
+        min_height = min(heights[left], heights[right])
+        current_amount = width * min_height
+
+        max_amount = max(max_amount, current_amount)
+
+        if heights[left] < heights[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_amount
+
+
+#################### Test Case ####################
+class TestMaxArea(unittest.TestCase):
+    def test_max_area(self):
+        self.assertEqual(max_area([1, 8, 6, 2, 5, 4, 8, 3, 7]), 49)
