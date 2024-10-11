4027c63148fb6545cb56f303e9f11745e0eca1ec
Atsushi <atu729@icloud.com>
2024-10-11 19:45:39 +0900

    add model solution

--- Python/000/011-container-with-most-water.py
+++ Python/000/011-container-with-most-water.py
@@ -21,7 +21,24 @@ def max_area(heights: list[int]) -> int:
     return max_amount
 
 
+def model_max_area(heights: list[int]) -> int:
+    l, r = 0, len(heights) - 1
+    res = 0
+
+    while l < r:
+        area = min(heights[l], heights[r]) * (r - l)
+        res = max(res, area)
+        if heights[l] <= heights[r]:
+            l += 1
+        else:
+            r -= 1
+    return res
+
+
 #################### Test Case ####################
 class TestMaxArea(unittest.TestCase):
     def test_max_area(self):
         self.assertEqual(max_area([1, 8, 6, 2, 5, 4, 8, 3, 7]), 49)
+
+    def test_model_max_area(self):
+        self.assertEqual(model_max_area([1, 8, 6, 2, 5, 4, 8, 3, 7]), 49)
