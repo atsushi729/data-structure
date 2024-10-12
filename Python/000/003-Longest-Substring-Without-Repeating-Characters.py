fdc9fcb323da0a5cb3d9ce76609e1518e621fffe
Atsushi <atu729@icloud.com>
2024-10-12 22:16:40 +0900

    add another solution

--- Python/000/003-Longest-Substring-Without-Repeating-Characters.py
+++ Python/000/003-Longest-Substring-Without-Repeating-Characters.py
@@ -19,6 +19,24 @@ def length_of_longest_substring(s: str) -> int:
     return current_max
 
 
+def length_of_longest_substring_v2(s: str) -> int:
+    if not s:
+        return 0
+
+    seen = {}
+    start = 0
+    longest = 0
+
+    for i, c in enumerate(s):
+        if c in seen and seen[c] >= start:
+            start = seen[c] + 1
+        else:
+            longest = max(longest, i - start + 1)
+        seen[c] = i
+
+    return longest
+
+
 #################### Test Case ####################
 class TestLengthOfLongestSubstring(unittest.TestCase):
     def test_length_of_longest_substring(self):
@@ -30,3 +48,13 @@ class TestLengthOfLongestSubstring(unittest.TestCase):
         self.assertEqual(length_of_longest_substring("au"), 2)
         self.assertEqual(length_of_longest_substring("dvdf"), 3)
         self.assertEqual(length_of_longest_substring("abba"), 2)
+
+    def test_length_of_longest_substring_v2(self):
+        self.assertEqual(length_of_longest_substring_v2("abcabcbb"), 3)
+        self.assertEqual(length_of_longest_substring_v2("bbbbb"), 1)
+        self.assertEqual(length_of_longest_substring_v2("pwwkew"), 3)
+        self.assertEqual(length_of_longest_substring_v2(""), 0)
+        self.assertEqual(length_of_longest_substring_v2(" "), 1)
+        self.assertEqual(length_of_longest_substring_v2("au"), 2)
+        self.assertEqual(length_of_longest_substring_v2("dvdf"), 3)
+        self.assertEqual(length_of_longest_substring_v2("abba"), 2)
