c74e797f2989c80e192f4851cf60f49996a9658f
Atsushi <atu729@icloud.com>
2024-10-14 23:55:38 +0900

    add model solution

--- Python/400/424-Longest-Repeating-Character-Replacement.py
+++ Python/400/424-Longest-Repeating-Character-Replacement.py
@@ -24,6 +24,22 @@ def character_replacement(s: str, k: int) -> int:
     return max_length
 
 
+def model_character_replacement(s: str, k: int) -> int:
+    count = {}
+    l = 0
+    maxf = 0
+
+    for r in range(len(s)):
+        count[s[r]] = 1 + count.get(s[r], 0)
+        maxf = max(maxf, count[s[r]])
+
+        if (r - l + 1) - maxf > k:
+            count[s[l]] -= 1
+            l += 1
+
+    return (r - l + 1)
+
+
 #################### Test Case ####################
 class TestCharacterReplacement(unittest.TestCase):
     def test_character_replacement(self):
@@ -31,3 +47,7 @@ class TestCharacterReplacement(unittest.TestCase):
         self.assertEqual(character_replacement("AABABBA", 1), 4)
         self.assertEqual(character_replacement("AABABBA", 0), 2)
 
+    def test_model_character_replacement(self):
+        self.assertEqual(model_character_replacement("ABAB", 2), 4)
+        self.assertEqual(model_character_replacement("AABABBA", 1), 4)
+        self.assertEqual(model_character_replacement("AABABBA", 0), 2)
