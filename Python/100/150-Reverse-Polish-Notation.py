d8889251a08bc9c21b99ffd9db73c9b63c0e841e
Atsushi <atu729@icloud.com>
2024-10-19 16:24:38 +0900

    add model solution

--- Python/100/150-Reverse-Polish-Notation.py
+++ Python/100/150-Reverse-Polish-Notation.py
@@ -25,8 +25,32 @@ def eval_rpn(tokens: list[str]) -> int:
     return stack[0]
 
 
+def model_eval_rpn(tokens: list[str]) -> int:
+    stack = []
+
+    for c in tokens:
+        if c == "+":
+            stack.append(stack.pop() + stack.pop())
+        elif c == "-":
+            a, b = stack.pop(), stack.pop()
+            stack.append(b - a)
+        elif c == "*":
+            stack.append(stack.pop() * stack.pop())
+        elif c == "/":
+            a, b = stack.pop(), stack.pop()
+            stack.append(int(float(b) / a))
+        else:
+            stack.append(int(c))
+    return stack[0]
+
+
 class TestEvalRPN(unittest.TestCase):
     def test_eval_rpn(self):
         self.assertEqual(eval_rpn(["2", "1", "+", "3", "*"]), 9)
         self.assertEqual(eval_rpn(["4", "13", "5", "/", "+"]), 6)
         self.assertEqual(eval_rpn(["10", "6", "9", "3", "/", "-11", "*", "/", "*", "17", "+", "5", "+"]), 22)
+
+    def test_model_eval_rpn(self):
+        self.assertEqual(model_eval_rpn(["2", "1", "+", "3", "*"]), 9)
+        self.assertEqual(model_eval_rpn(["4", "13", "5", "/", "+"]), 6)
+        self.assertEqual(model_eval_rpn(["10", "6", "9", "3", "/", "-11", "*", "/", "*", "17", "+", "5", "+"]), 22)
