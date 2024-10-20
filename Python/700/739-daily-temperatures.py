9f98a3a91569b972d0ac062220fd23f233cc8d94
Atsushi <atu729@icloud.com>
2024-10-20 14:27:44 +0900

    add model solution

--- Python/700/739-daily-temperatures.py
+++ Python/700/739-daily-temperatures.py
@@ -2,7 +2,7 @@ import unittest
 
 
 #################### Solution ####################
-def daily_temperatures(self, temperatures: list[int]) -> list[int]:
+def daily_temperatures(temperatures: list[int]) -> list[int]:
     result = []
 
     for i in range(len(temperatures)):
@@ -25,9 +25,29 @@ def daily_temperatures(self, temperatures: list[int]) -> list[int]:
     return result
 
 
+def model_daily_temperatures(temperatures: [int]) -> [int]:
+    n = len(temperatures)  # Get the length of the temperatures list
+    answer = [0] * n
+    stack = []
+
+    for i in range(n):
+        # While the stack is not empty and the current temperature is higher than the temperature at the top of the
+        # stack
+        while stack and temperatures[i] > temperatures[stack[-1]]:
+            j = stack.pop()  # Pop the index of the top element from the stack
+            answer[j] = i - j  # Record the number of days in the answer list
+        stack.append(i)  # Append the current index to the stack
+    return answer
+
+
 #################### Test Case ####################
 class TestDailyTemperature(unittest.TestCase):
     def test_daily_temperatures(self):
         self.assertEqual(daily_temperatures([73, 74, 75, 71, 69, 72, 76, 73]), [1, 1, 4, 2, 1, 1, 0, 0])
         self.assertEqual(daily_temperatures([30, 40, 50, 60]), [1, 1, 1, 0])
         self.assertEqual(daily_temperatures([30, 60, 90]), [1, 1, 0])
+
+    def test_model_daily_temperatures(self):
+        self.assertEqual(model_daily_temperatures([73, 74, 75, 71, 69, 72, 76, 73]), [1, 1, 4, 2, 1, 1, 0, 0])
+        self.assertEqual(model_daily_temperatures([30, 40, 50, 60]), [1, 1, 1, 0])
+        self.assertEqual(model_daily_temperatures([30, 60, 90]), [1, 1, 0])
