10bfc4d6caa47623a87b81e8273bf1919f02eebf
Atsushi <atu729@icloud.com>
2024-10-20 21:13:47 +0900

    add model solution

--- Python/800/853-car-fleet.py
+++ Python/800/853-car-fleet.py
@@ -17,7 +17,27 @@ def car_fleet(target: int, position: list[int], speed: list[int]) -> int:
     return len(stack)
 
 
+def model_car_fleet(target: int, position: list[int], speed: list[int]) -> int:
+    pair = [(p, s) for p, s in zip(position, speed)]
+    pair.sort(reverse=True)
+    stack = []
+
+    for p, s in pair:
+        time = (target - p) / s
+
+        if not stack:
+            stack.append(time)
+        else:
+            if time > stack[-1]:
+                stack.append(time)
+
+    return len(stack)
+
+
 #################### Test Case ####################
 class TestCarFleet(unittest.TestCase):
     def test_car_fleet(self):
         self.assertEqual(car_fleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]), 3)
+
+    def test_model_car_fleet(self):
+        self.assertEqual(model_car_fleet(12, [10, 8, 0, 5, 3], [2, 4, 1, 1, 3]), 3)
