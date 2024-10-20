65149403ce131fe0e8bc9308b530691703123351
Atsushi <atu729@icloud.com>
2024-10-20 21:38:24 +0900

    add brute force solution

---
+++ Python/000/074-search-a-2D-matrix.py
@@ -0,0 +1,35 @@
+import unittest
+
+
+#################### Solution ####################
+def search_matrix(matrix: list[list[int]], target: int) -> bool:
+    reasonable_row = []
+    left, right = 0, len(matrix[0]) - 1
+
+    # find reasonable row
+    for row in matrix:
+        if row[0] <= target <= row[-1]:
+            reasonable_row = row
+            break
+
+    # if reasonable row is empty, return False
+    if len(reasonable_row) == 0:
+        return False
+
+    # find target from row
+    while left <= right:
+        middle = (left + right) // 2
+        if reasonable_row[middle] == target:
+            return True
+        elif reasonable_row[middle] < target:
+            left = middle + 1
+        else:
+            right = middle - 1
+
+    return False
+
+
+#################### Test Case ####################
+class TestSearchMatrix(unittest.TestCase):
+    def test_search_matrix(self):
+        self.assertEqual(search_matrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3), True)
