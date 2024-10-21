36499cd5bba61fe7b9c68d84c73dcc11143c00c9
Atsushi <atu729@icloud.com>
2024-10-21 12:46:39 +0900

    add model solution

--- Python/000/074-search-a-2D-matrix.py
+++ Python/000/074-search-a-2D-matrix.py
@@ -29,7 +29,35 @@ def search_matrix(matrix: list[list[int]], target: int) -> bool:
     return False
 
 
+def model_search_matrix(matrix: list[list[int]], target: int) -> bool:
+
+    if not matrix or not matrix[0]:
+        return False
+
+    m, n = len(matrix), len(matrix[0])
+    left, right = 0, m * n - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        row = mid // n
+        col = mid % n
+        mid_value = matrix[row][col]
+
+        if mid_value == target:
+            return True
+        elif mid_value < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return False
+
+
 #################### Test Case ####################
 class TestSearchMatrix(unittest.TestCase):
     def test_search_matrix(self):
         self.assertEqual(search_matrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3), True)
+
+    def test_model_search_matrix(self):
+        self.assertEqual(model_search_matrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3), True)
+        self.assertEqual(model_search_matrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 13), False)
