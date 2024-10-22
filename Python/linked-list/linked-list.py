d06dbfdc339636eace78b8bc3f45d793db06e2f7
Atsushi <atu729@icloud.com>
2024-10-22 20:25:51 +0900

    add linked list operation

--- Python/linked-list/linked-list.py
+++ Python/linked-list/linked-list.py
@@ -6,12 +6,40 @@ class Node:
 
 class LinkedList:
     def __init__(self) -> None:
-        self.haed_value = None
+        self.head = None
+
+    def append(self, data: None) -> None:
+        new_node = Node(data)
+
+        # If the linked list is empty, then make the new node the head.
+        if self.head is None:
+            self.head = new_node
+            return
+
+        # Traverse the linked list to the last node.
+        last_node = self.head
+        while last_node.next_value:
+            last_node = last_node.next_value
+        last_node.next_value = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next_value
 
 
-list1 = LinkedList()
-list1.haed_value = Node("mon")
-element2 = Node("tue")
-element3 = Node("wed")
-list1.haed_value.next_value = element2
-element2.next_value = element3
+if __name__ == '__main__':
+    linked_list = LinkedList()
+    linked_list.append("A")
+    linked_list.append("B")
+    linked_list.append("C")
+    linked_list.append("D")
+    linked_list.append("E")
+    print(linked_list.head)
+    print(linked_list.head.next_value)
+    print(linked_list.head.next_value.next_value)
+    print(linked_list.head.next_value.next_value.next_value)
+    print(linked_list.head.next_value.next_value.next_value.next_value)
+    print(linked_list.head.next_value.next_value.next_value.next_value.next_value)
+    linked_list.display()
