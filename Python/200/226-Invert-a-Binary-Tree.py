8a250ddc23df57f4544ca5278601c30ffb43b20e
Atsushi <atu729@icloud.com>
2024-10-26 23:20:15 +0900

    add another solution

--- Python/200/226-Invert-a-Binary-Tree.py
+++ Python/200/226-Invert-a-Binary-Tree.py
@@ -23,6 +23,15 @@ class Solution:
 
         return root
 
+    def invert_tree_v2(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # base case
+        if not root:
+            return None
+
+        root.left, root.right = self.invert_tree_v2(root.right), self.invert_tree_v2(root.left)
+
+        return root
+
 
 #################### Test Case ####################
 class TestInvertTree(unittest.TestCase):
@@ -37,6 +46,26 @@ class TestInvertTree(unittest.TestCase):
 
         solution = Solution()
         result = solution.invert_tree(root)
+
+        self.assertEqual(result.val, 4)
+        self.assertEqual(result.left.val, 7)
+        self.assertEqual(result.right.val, 2)
+        self.assertEqual(result.left.left.val, 9)
+        self.assertEqual(result.left.right.val, 6)
+        self.assertEqual(result.right.left.val, 3)
+        self.assertEqual(result.right.right.val, 1)
+
+    def test_inverse_tree_v2(self):
+        root = TreeNode(4)
+        root.left = TreeNode(2)
+        root.right = TreeNode(7)
+        root.left.left = TreeNode(1)
+        root.left.right = TreeNode(3)
+        root.right.left = TreeNode(6)
+        root.right.right = TreeNode(9)
+
+        solution = Solution()
+        result = solution.invert_tree_v2(root)
 
         self.assertEqual(result.val, 4)
         self.assertEqual(result.left.val, 7)
