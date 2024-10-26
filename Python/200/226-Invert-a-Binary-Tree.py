8bf86e139caeacd99317aeb27f82f4c95159f624
Atsushi <atu729@icloud.com>
2024-10-26 11:55:52 +0900

    add invert a binary tree algorithm

---
+++ Python/200/226-Invert-a-Binary-Tree.py
@@ -0,0 +1,47 @@
+from typing import Optional
+import unittest
+
+
+#################### Solution ####################
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invert_tree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        # base case
+        if not root:
+            return None
+
+        root.left, root.right = root.right, root.left
+
+        self.invert_tree(root.left)
+        self.invert_tree(root.right)
+
+        return root
+
+
+#################### Test Case ####################
+class TestInvertTree(unittest.TestCase):
+    def test_invert_tree(self):
+        root = TreeNode(4)
+        root.left = TreeNode(2)
+        root.right = TreeNode(7)
+        root.left.left = TreeNode(1)
+        root.left.right = TreeNode(3)
+        root.right.left = TreeNode(6)
+        root.right.right = TreeNode(9)
+
+        solution = Solution()
+        result = solution.invert_tree(root)
+
+        self.assertEqual(result.val, 4)
+        self.assertEqual(result.left.val, 7)
+        self.assertEqual(result.right.val, 2)
+        self.assertEqual(result.left.left.val, 9)
+        self.assertEqual(result.left.right.val, 6)
+        self.assertEqual(result.right.left.val, 3)
+        self.assertEqual(result.right.right.val, 1)
