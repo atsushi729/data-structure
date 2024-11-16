36440b1e485de7a18b554614aa924d65b7ae31d1
Atsushi <atu729@icloud.com>
2024-11-16 18:15:12 +0900

    add another solution

--- Python/1000/1071-Greatest-Common-Divisor-of-Strings.py
+++ Python/1000/1071-Greatest-Common-Divisor-of-Strings.py
@@ -8,8 +8,27 @@ def gcd_of_strings(self, str1: str, str2: str) -> str:
     return str1[:math.gcd(len(str1), len(str2))]
 
 
+def gcd_of_strings_v2(str1: str, str2: str) -> str:
+    if str1 + str2 != str2 + str1:
+        return ""
+
+    gcd_length = math.gcd(len(str1), len(str2))
+
+    candidate_str = str1[:gcd_length]
+
+    if candidate_str * (len(str1) // gcd_length) == str1 and candidate_str * (len(str2) // gcd_length) == str2:
+        return candidate_str
+    else:
+        return ""
+
+
 class TestGcdOfStrings(unittest.TestCase):
     def test_gcd_of_strings(self):
         self.assertEqual(gcd_of_strings(self, "ABCABC", "ABC"), "ABC")
         self.assertEqual(gcd_of_strings(self, "ABABAB", "ABAB"), "AB")
         self.assertEqual(gcd_of_strings(self, "LEET", "CODE"), "")
+
+    def test_gcd_of_strings_v2(self):
+        self.assertEqual(gcd_of_strings_v2("ABCABC", "ABC"), "ABC")
+        self.assertEqual(gcd_of_strings_v2("ABABAB", "ABAB"), "AB")
+        self.assertEqual(gcd_of_strings_v2("LEET", "CODE"), "")
