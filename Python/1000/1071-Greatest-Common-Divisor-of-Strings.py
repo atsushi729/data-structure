b0eeb589f55b18e83dc3fe7dc611463be019add7
Atsushi <atu729@icloud.com>
2024-11-16 18:10:43 +0900

    fix function name

--- Python/1000/1071-Greatest-Common-Divisor-of-Strings.py
+++ Python/1000/1071-Greatest-Common-Divisor-of-Strings.py
@@ -2,7 +2,7 @@ import math
 import unittest
 
 
-def gcdOfStrings(self, str1: str, str2: str) -> str:
+def gcd_of_strings(self, str1: str, str2: str) -> str:
     if str1 + str2 != str2 + str1:
         return ""
     return str1[:math.gcd(len(str1), len(str2))]
@@ -10,6 +10,6 @@ def gcdOfStrings(self, str1: str, str2: str) -> str:
 
 class TestGcdOfStrings(unittest.TestCase):
     def test_gcd_of_strings(self):
-        self.assertEqual(gcdOfStrings(self, "ABCABC", "ABC"), "ABC")
-        self.assertEqual(gcdOfStrings(self, "ABABAB", "ABAB"), "AB")
-        self.assertEqual(gcdOfStrings(self, "LEET", "CODE"), "")
+        self.assertEqual(gcd_of_strings(self, "ABCABC", "ABC"), "ABC")
+        self.assertEqual(gcd_of_strings(self, "ABABAB", "ABAB"), "AB")
+        self.assertEqual(gcd_of_strings(self, "LEET", "CODE"), "")
