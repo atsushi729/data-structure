ac96d9f9ec49f9b93e81717068b57ba369c977d3
Atsushi <atu729@icloud.com>
2024-11-17 19:07:29 +0900

    add another solution

--- Python/1400/1431-Kids-With-the-Greatest-Number-of-Candies.py
+++ Python/1400/1431-Kids-With-the-Greatest-Number-of-Candies.py
@@ -14,9 +14,19 @@ def kids_with_candies(candies: list[int], extraCandies: int) -> list[bool]:
     return answer
 
 
+def kids_with_candies_v2(candies: list[int], extraCandies: int) -> list[bool]:
+    max_candy = max(candies)
+    return [candy + extraCandies >= max_candy for candy in candies]
+
+
 #################### Test Case ####################
 class TestKidsWithCandies(unittest.TestCase):
     def test_kids_with_candies(self):
         self.assertEqual(kids_with_candies([2, 3, 5, 1, 3], 3), [True, True, True, False, True])
         self.assertEqual(kids_with_candies([4, 2, 1, 1, 2], 1), [True, False, False, False, False])
         self.assertEqual(kids_with_candies([12, 1, 12], 10), [True, False, True])
+
+    def test_kids_with_candies_v2(self):
+        self.assertEqual(kids_with_candies_v2([2, 3, 5, 1, 3], 3), [True, True, True, False, True])
+        self.assertEqual(kids_with_candies_v2([4, 2, 1, 1, 2], 1), [True, False, False, False, False])
+        self.assertEqual(kids_with_candies_v2([12, 1, 12], 10), [True, False, True])
