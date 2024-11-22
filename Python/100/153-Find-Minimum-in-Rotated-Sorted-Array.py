68079fa7a8f19a7e6139c813f1f41c446efc261c
Atsushi <atu729@icloud.com>
2024-11-22 23:42:17 +0900

    add another solution

--- Python/100/153-Find-Minimum-in-Rotated-Sorted-Array.py
+++ Python/100/153-Find-Minimum-in-Rotated-Sorted-Array.py
@@ -21,6 +21,10 @@ def find_min(nums: list[int]) -> int:
     return min(curr_min, nums[start])
 
 
+def find_min_v2(nums: list[int]) -> int:
+    return min(nums)
+
+
 #################### Test Case ####################
 class TestFindMin(unittest.TestCase):
     def test_findMin(self):
@@ -29,3 +33,10 @@ class TestFindMin(unittest.TestCase):
         self.assertEqual(find_min([11, 13, 15, 17]), 11)
         self.assertEqual(find_min([2, 1]), 1)
         self.assertEqual(find_min([1]), 1)
+
+    def test_findMin_v2(self):
+        self.assertEqual(find_min_v2([3, 4, 5, 1, 2]), 1)
+        self.assertEqual(find_min_v2([4, 5, 6, 7, 0, 1, 2]), 0)
+        self.assertEqual(find_min_v2([11, 13, 15, 17]), 11)
+        self.assertEqual(find_min_v2([2, 1]), 1)
+        self.assertEqual(find_min_v2([1]), 1)
