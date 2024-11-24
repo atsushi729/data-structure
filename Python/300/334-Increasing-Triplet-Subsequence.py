d7a643672744eaed1399788e76c2577ee2dbae0f
Atsushi <atu729@icloud.com>
2024-11-24 15:46:07 +0900

    add incresing triplet

---
+++ Python/300/334-Increasing-Triplet-Subsequence.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class Solution:
+    def increasing_triplet(self, nums: list[int]) -> bool:
+        first = second = float('inf')
+
+        for num in nums:
+            if num <= first:
+                first = num  # 最小値を更新
+            elif num <= second:
+                second = num  # 中間値を更新
+            else:
+                return True
+        return False
+
+
+#################### Test Case ####################
+class TestIncreasingTriplet(unittest.TestCase):
+    def test_increasing_triplet(self):
+        nums = [1, 2, 3, 4, 5]
+        self.assertTrue(Solution().increasingTriplet(nums))
+        nums = [2, 4, -2, -3]
+        self.assertFalse(Solution().increasing_triplet(nums))
