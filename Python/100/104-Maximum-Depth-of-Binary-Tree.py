acf2a262c6af335015489a6a18df95551db9af3c
Atsushi <atu729@icloud.com>
2024-11-30 23:54:35 +0900

    add Maximum Depth of Binary Tree

---
+++ Python/100/104-Maximum-Depth-of-Binary-Tree.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+from collections import deque
+from typing import Optional
+import unittest
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        q = deque()
+        if root:
+            q.append(root)
+
+        level = 0
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+
+        return level
+
+
+class TestMaxDepth(unittest.TestCase):
+    def test_max_depth(self):
+        root = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+        self.assertEqual(Solution().maxDepth(root), 3)
+
+        root = TreeNode(1, None, TreeNode(2))
+        self.assertEqual(Solution().maxDepth(root), 2)
+
+        root = None
+        self.assertEqual(Solution().maxDepth(root), 0)
