db12c647f0c1cacec46bacf8cf378133bf1aa5c7
Atsushi <atu729@icloud.com>
2024-12-03 23:15:29 +0900

    add bfs

--- Python/100/100-Same-Tree.py
+++ Python/100/100-Same-Tree.py
@@ -18,6 +18,26 @@ class Solution:
             return False
         return self.is_same_tree(p.left, q.left) and self.is_same_tree(p.right, q.right)
 
+    def is_same_tree_bfs(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+
+        queue = [(p, q)]
+
+        while queue:
+            node1, node2 = queue.pop(0)
+            if not node1 and not node2:
+                continue
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+
+            queue.append((node1.left, node2.left))
+            queue.append((node1.right, node2.right))
+
+        return True
+
 
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
@@ -33,3 +53,16 @@ class TestSolution(unittest.TestCase):
         p = TreeNode(1, TreeNode(2), TreeNode(1))
         q = TreeNode(1, TreeNode(1), TreeNode(2))
         self.assertEqual(Solution().is_same_tree(p, q), False)
+
+    def test_is_same_tree_bfs(self):
+        p = TreeNode(1, TreeNode(2), TreeNode(3))
+        q = TreeNode(1, TreeNode(2), TreeNode(3))
+        self.assertEqual(Solution().is_same_tree_bfs(p, q), True)
+
+        p = TreeNode(1, TreeNode(2))
+        q = TreeNode(1, None, TreeNode(2))
+        self.assertEqual(Solution().is_same_tree_bfs(p, q), False)
+
+        p = TreeNode(1, TreeNode(2), TreeNode(1))
+        q = TreeNode(1, TreeNode(1), TreeNode(2))
+        self.assertEqual(Solution().is_same_tree_bfs(p, q), False)
