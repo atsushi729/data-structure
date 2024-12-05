90fe585f1632ec292dde764dc824949e52e38e04
Atsushi <atu729@icloud.com>
2024-12-05 23:33:37 +0900

    add stack method

--- Python/500/543-Diameter-of-Binary-Tree.py
+++ Python/500/543-Diameter-of-Binary-Tree.py
@@ -56,6 +56,28 @@ class Solution:
         dfs(root)
         return self.res
 
+    def diameter_of_binary_tree_v3(self, root: Optional[TreeNode]) -> int:
+        stack = [root]
+        mp = {None: (0, 0)}
+
+        while stack:
+            node = stack[-1]
+
+            if node.left and node.left not in mp:
+                stack.append(node.left)
+            elif node.right and node.right not in mp:
+                stack.append(node.right)
+            else:
+                node = stack.pop()
+
+                leftHeight, leftDiameter = mp[node.left]
+                rightHeight, rightDiameter = mp[node.right]
+
+                mp[node] = (1 + max(leftHeight, rightHeight),
+                            max(leftHeight + rightHeight, leftDiameter, rightDiameter))
+
+        return mp[root][1]
+
 
 #################### Test Case ####################
 class TestDiameterOfBinaryTree(unittest.TestCase):
@@ -80,5 +102,17 @@ class TestDiameterOfBinaryTree(unittest.TestCase):
 
         solution = Solution()
         result = solution.diameter_of_binary_tree_v2(root)
+
+        self.assertEqual(result, 3)
+
+    def test_diameter_of_binary_tree_v3(self):
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+        root.left.left = TreeNode(4)
+        root.left.right = TreeNode(5)
+
+        solution = Solution()
+        result = solution.diameter_of_binary_tree_v3(root)
 
         self.assertEqual(result, 3)
