59f93a3f0e0ff5ddc1ac5dcb5e1a15df13345382
Atsushi <atu729@icloud.com>
2024-12-06 19:23:36 +0900

    add iteration method

--- Python/200/235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
+++ Python/200/235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
@@ -21,6 +21,15 @@ class Solution:
             return self.lowest_common_ancestor(root.right, p, q)
         return root
 
+    def lowest_common_ancestor_v2(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if p.val < root.val > q.val:
+                root = root.left
+            elif p.val > root.val < q.val:
+                root = root.right
+            else:
+                return root
+
 
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
@@ -46,3 +55,26 @@ class TestSolution(unittest.TestCase):
         p = root.left
         q = root.left.right.right
         self.assertEqual(Solution().lowest_common_ancestor(root, p, q).val, 2)
+
+    def test_lowest_common_ancestor_v2(self):
+        root = TreeNode(6)
+        root.left = TreeNode(2)
+        root.right = TreeNode(8)
+        root.left.left = TreeNode(0)
+        root.left.right = TreeNode(4)
+        root.right.left = TreeNode(7)
+        root.right.right = TreeNode(9)
+        root.left.right.left = TreeNode(3)
+        root.left.right.right = TreeNode(5)
+
+        p = root.left
+        q = root.right
+        self.assertEqual(Solution().lowest_common_ancestor_v2(root, p, q).val, 6)
+
+        p = root.left
+        q = root.left.right
+        self.assertEqual(Solution().lowest_common_ancestor_v2(root, p, q).val, 2)
+
+        p = root.left
+        q = root.left.right.right
+        self.assertEqual(Solution().lowest_common_ancestor_v2(root, p, q).val, 2)
