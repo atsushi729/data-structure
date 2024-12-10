c5cc937eba7f5c15274bd38254fde0d6d2849fbc
Atsushi <atu729@icloud.com>
2024-12-10 19:54:58 +0900

    add stack approach

--- Python/200/230-Kth-Smallest-Element-in-a-BST.py
+++ Python/200/230-Kth-Smallest-Element-in-a-BST.py
@@ -28,6 +28,23 @@ class Solution:
 
         return node_list[k - 1]
 
+    def kth_smallest2(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        node = root
+
+        while True:
+            while node:
+                stack.append(node)
+                node = node.left
+
+            node = stack.pop()
+            k -= 1
+
+            if k == 0:
+                return node.val
+
+            node = node.right
+
 
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
@@ -37,3 +54,10 @@ class TestSolution(unittest.TestCase):
 
         root = TreeNode(5, TreeNode(3, TreeNode(2, TreeNode(1)), TreeNode(4)), TreeNode(6))
         self.assertEqual(Solution().kth_smallest(root, 3), 3)
+
+    def test_kthSmallest2(self):
+        root = TreeNode(3, TreeNode(1, None, TreeNode(2)), TreeNode(4))
+        self.assertEqual(Solution().kth_smallest2(root, 1), 1)
+
+        root = TreeNode(5, TreeNode(3, TreeNode(2, TreeNode(1)), TreeNode(4)), TreeNode(6))
+        self.assertEqual(Solution().kth_smallest2(root, 3), 3)
