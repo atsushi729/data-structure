c60c47fefdbdee35fb2dd9a215b0f8608eb61dfd
Atsushi <atu729@icloud.com>
2024-12-10 19:40:33 +0900

    add kth smallest element in a BST

---
+++ Python/200/230-Kth-Smallest-Element-in-a-BST.py
@@ -0,0 +1,39 @@
+from typing import Optional
+import unittest
+
+
+#################### Solution ####################
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        node_list = []
+
+        def dfs(node):
+            nonlocal node_list
+
+            if not node:
+                return None
+
+            dfs(node.left)
+            node_list.append(node.val)
+            dfs(node.right)
+
+        dfs(root)
+
+        return node_list[k - 1]
+
+
+#################### Test Case ####################
+class TestSolution(unittest.TestCase):
+    def test_kthSmallest(self):
+        root = TreeNode(3, TreeNode(1, None, TreeNode(2)), TreeNode(4))
+        self.assertEqual(Solution().kthSmallest(root, 1), 1)
+
+        root = TreeNode(5, TreeNode(3, TreeNode(2, TreeNode(1)), TreeNode(4)), TreeNode(6))
+        self.assertEqual(Solution().kthSmallest(root, 3), 3)
