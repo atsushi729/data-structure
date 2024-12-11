05a81b0f6d09f744e0ff149a71b4be0bcacaa96e
Atsushi <atu729@icloud.com>
2024-12-11 19:59:28 +0900

    add build tree

---
+++ Python/100/105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py
@@ -0,0 +1,43 @@
+from typing import List, Optional
+import unittest
+
+
+#################### Solution ####################
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def build_tree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+
+        root.left = self.build_tree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.build_tree(preorder[mid + 1:], inorder[mid + 1:])
+
+        return root
+
+
+#################### Test Case ####################
+class TestSolution(unittest.TestCase):
+    def compareTrees(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.compareTrees(p.left, q.left) and self.compareTrees(p.right, q.right)
+
+    def test_buildTree(self):
+        preorder = [3, 9, 20, 15, 7]
+        inorder = [9, 3, 15, 20, 7]
+        expected_root = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+        result_root = Solution().build_tree(preorder, inorder)
+        self.assertTrue(self.compareTrees(result_root, expected_root))
