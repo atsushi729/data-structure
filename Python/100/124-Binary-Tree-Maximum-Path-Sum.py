4a9f99945fdeb5cd111f9ff6cd80cae7854de357
Atsushi <atu729@icloud.com>
2024-12-12 22:43:57 +0900

    add test code

--- Python/100/124-Binary-Tree-Maximum-Path-Sum.py
+++ Python/100/124-Binary-Tree-Maximum-Path-Sum.py
@@ -1,14 +1,18 @@
+import unittest
 from typing import Optional
 
+
+#################### Solution ####################
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        res = -float("int")
+        res = -float("inf")
 
         def dfs(node):
             nonlocal res
@@ -24,3 +28,17 @@ class Solution:
         dfs(root)
         return res
 
+
+#################### Test Case ####################
+class TestSolution(unittest.TestCase):
+    def test_max_path_sum(self):
+        root = TreeNode(1)
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+        self.assertEqual(Solution().maxPathSum(root), 6)
+        root = TreeNode(-10)
+        root.left = TreeNode(9)
+        root.right = TreeNode(20)
+        root.right.left = TreeNode(15)
+        root.right.right = TreeNode(7)
+        self.assertEqual(Solution().maxPathSum(root), 42)
