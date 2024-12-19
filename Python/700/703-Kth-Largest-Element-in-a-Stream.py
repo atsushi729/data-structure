b329fad96fb3b9d675abe2451dc45e54ca0629b6
Atsushi <atu729@icloud.com>
2024-12-19 20:41:04 +0900

    add heap approach

--- Python/700/703-Kth-Largest-Element-in-a-Stream.py
+++ Python/700/703-Kth-Largest-Element-in-a-Stream.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import List
 import unittest
 
@@ -15,10 +16,34 @@ class KthLargest:
         return self.nums[len(self.nums) - self.k]
 
 
+class KthLargestV2:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.minHeap = nums
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+
+
 #################### Test Case ####################
 class TestKthLargest(unittest.TestCase):
     def test_add(self):
         kth_largest = KthLargest(3, [4, 5, 8, 2])
+        self.assertEqual(kth_largest.add(3), 4)
+        self.assertEqual(kth_largest.add(5), 5)
+        self.assertEqual(kth_largest.add(10), 5)
+        self.assertEqual(kth_largest.add(9), 8)
+        self.assertEqual(kth_largest.add(4), 8)
+
+    def test_add_v2(self):
+        kth_largest = KthLargestV2(3, [4, 5, 8, 2])
         self.assertEqual(kth_largest.add(3), 4)
         self.assertEqual(kth_largest.add(5), 5)
         self.assertEqual(kth_largest.add(10), 5)
