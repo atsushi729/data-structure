19784cc2842fcf3d5ec5c88901abc9ce27fa4d7d
Atsushi <atu729@icloud.com>
2024-12-21 16:32:09 +0900

    add heap solution

--- Python/1000/1046-Last-Stone-Weight.py
+++ Python/1000/1046-Last-Stone-Weight.py
@@ -1,5 +1,7 @@
 from typing import List
 import unittest
+import heapq
+
 
 class Solution:
     def last_stone_weight(self, stones: List[int]) -> int:
@@ -12,6 +14,15 @@ class Solution:
 
         return stones[0] if stones else 0
 
+    def last_stone_weight_v2(self, stones: List[int]) -> int:
+        stones = [-x for x in stones]
+        heapq.heapify(stones)
+        while len(stones) > 1:
+            x, y = heapq.heappop(stones), heapq.heappop(stones)
+            if x != y:
+                heapq.heappush(stones, x - y)
+        return -stones[0] if stones else 0
+
 
 class TestSolution(unittest.TestCase):
     def test_lastStoneWeight(self):
@@ -23,3 +34,11 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(solution.last_stone_weight([2, 2, 2, 2, 2]), 2)
         self.assertEqual(solution.last_stone_weight([2, 2, 2, 2, 2, 2]), 0)
 
+    def test_lastStoneWeightV2(self):
+        solution = Solution()
+        self.assertEqual(solution.last_stone_weight_v2([2, 7, 4, 1, 8, 1]), 1)
+        self.assertEqual(solution.last_stone_weight_v2([2, 2]), 0)
+        self.assertEqual(solution.last_stone_weight_v2([2, 2, 2]), 2)
+        self.assertEqual(solution.last_stone_weight_v2([2, 2, 2, 2]), 0)
+        self.assertEqual(solution.last_stone_weight_v2([2, 2, 2, 2, 2]), 2)
+        self.assertEqual(solution.last_stone_weight_v2([2, 2, 2, 2, 2, 2]), 0)
