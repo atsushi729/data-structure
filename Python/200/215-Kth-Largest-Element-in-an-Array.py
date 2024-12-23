e42a2efc4633cd5998d35afe4a3999ddcad067fa
Atsushi <atu729@icloud.com>
2024-12-24 00:36:21 +0900

    add quick select approach

--- Python/200/215-Kth-Largest-Element-in-an-Array.py
+++ Python/200/215-Kth-Largest-Element-in-an-Array.py
@@ -16,6 +16,26 @@ class Solution:
                 heapq.heappop(min_heap)
         return min_heap[0]
 
+    def find_kth_largest_v3(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k
+
+        def quick_select(l, r):
+            pivot, p = nums[r], l
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[p], nums[i] = nums[i], nums[p]
+                    p += 1
+            nums[p], nums[r] = nums[r], nums[p]
+
+            if p > k:
+                return quick_select(l, p - 1)
+            elif p < k:
+                return quick_select(p + 1, r)
+            else:
+                return nums[p]
+
+        return quick_select(0, len(nums) - 1)
+
 
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
@@ -28,3 +48,8 @@ class TestSolution(unittest.TestCase):
         solution = Solution()
         self.assertEqual(solution.find_kth_largest_v2([3, 2, 1, 5, 6, 4], 2), 5)
         self.assertEqual(solution.find_kth_largest_v2([3, 2, 3, 1, 2, 4, 5, 5, 6], 4), 4)
+
+    def test_find_kth_largest_v3(self):
+        solution = Solution()
+        self.assertEqual(solution.find_kth_largest_v3([3, 2, 1, 5, 6, 4], 2), 5)
+        self.assertEqual(solution.find_kth_largest_v3([3, 2, 3, 1, 2, 4, 5, 5, 6], 4), 4)
