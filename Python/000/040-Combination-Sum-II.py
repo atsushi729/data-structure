fd7d015834c8bf95ee68f13f4deb6ddc4a9c3d92
Atsushi <atu729@icloud.com>
2025-01-05 13:36:01 +0900

    Update comments

--- Python/000/040-Combination-Sum-II.py
+++ Python/000/040-Combination-Sum-II.py
@@ -1,8 +1,10 @@
 from typing import List
 import unittest
 
+
+#################### Solution ####################
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combination_sum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
         candidates.sort()
 
@@ -13,9 +15,12 @@ class Solution:
             if i >= len(candidates) or total > target:
                 return
 
+            # Include the current number
             cur.append(candidates[i])
             dfs(i + 1, cur, total + candidates[i])
             cur.pop()
+
+            # Skip the same number (Not include the current number)
             while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
                 i += 1
             dfs(i + 1, cur, total)
@@ -24,8 +29,10 @@ class Solution:
         return res
 
 
+#################### Test Case ####################
 class TestSolution(unittest.TestCase):
     def test_CombinationSum2(self):
         solution = Solution()
-        self.assertListEqual(solution.combinationSum2([10, 1, 2, 7, 6, 1, 5], 8), [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]])
-        self.assertListEqual(solution.combinationSum2([2, 5, 2, 1, 2], 5), [[1, 2, 2], [5]])
+        self.assertListEqual(solution.combination_sum2([10, 1, 2, 7, 6, 1, 5], 8),
+                             [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]])
+        self.assertListEqual(solution.combination_sum2([2, 5, 2, 1, 2], 5), [[1, 2, 2], [5]])
