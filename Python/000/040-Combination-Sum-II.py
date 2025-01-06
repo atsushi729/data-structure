8dbc4fb4a51867604d6d6630316127773ce01e2b
Atsushi <atu729@icloud.com>
2025-01-06 18:41:54 +0900

    add brute force approach

--- Python/000/040-Combination-Sum-II.py
+++ Python/000/040-Combination-Sum-II.py
@@ -28,11 +28,47 @@ class Solution:
         dfs(0, [], 0)
         return res
 
+    def combination_sum2_v2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = set()
+        candidates.sort()
+
+        def dfs(i, cur, total):
+            if total == target:
+                res.add(tuple(cur))
+                return
+            if i >= len(candidates) or total > target:
+                return
+
+            # Include the current number
+            cur.append(candidates[i])
+            dfs(i + 1, cur, total + candidates[i])
+            cur.pop()
+            dfs(i + 1, cur, total)
+
+        dfs(0, [], 0)
+        return [list(r) for r in res]
+
 
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
     def test_CombinationSum2(self):
         solution = Solution()
-        self.assertListEqual(solution.combination_sum2([10, 1, 2, 7, 6, 1, 5], 8),
-                             [[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]])
-        self.assertListEqual(solution.combination_sum2([2, 5, 2, 1, 2], 5), [[1, 2, 2], [5]])
+        self.assertListEqual(
+            sorted(solution.combination_sum2_v2([10, 1, 2, 7, 6, 1, 5], 8)),
+            sorted([[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]])
+        )
+        self.assertListEqual(
+            sorted(solution.combination_sum2_v2([2, 5, 2, 1, 2], 5)),
+            sorted([[1, 2, 2], [5]])
+        )
+
+    def test_combinationSum2_v2(self):
+        solution = Solution()
+        self.assertListEqual(
+            sorted(solution.combination_sum2_v2([10, 1, 2, 7, 6, 1, 5], 8)),
+            sorted([[1, 1, 6], [1, 2, 5], [1, 7], [2, 6]])
+        )
+        self.assertListEqual(
+            sorted(solution.combination_sum2_v2([2, 5, 2, 1, 2], 5)),
+            sorted([[1, 2, 2], [5]])
+        )
