b954e19d1c4a2bf6cbeedcacd80b0175874b0ca6
Atsushi <atu729@icloud.com>
2025-01-13 18:52:54 +0900

    add word search with visited array

--- Python/000/079-Word-Search.py
+++ Python/000/079-Word-Search.py
@@ -26,10 +26,40 @@ class Solution:
                     return True
         return False
 
+    def exist_v2(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        visited = [[False for _ in range(COLS)] for _ in range(ROWS)]
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or
+                    word[i] != board[r][c] or visited[r][c]):
+                return False
+
+            visited[r][c] = True
+            res = (dfs(r + 1, c, i + 1) or
+                   dfs(r - 1, c, i + 1) or
+                   dfs(r, c + 1, i + 1) or
+                   dfs(r, c - 1, i + 1))
+            visited[r][c] = False
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if dfs(r, c, 0):
+                    return True
+        return False
+
 
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
     def test_exist(self):
         solution = Solution()
         self.assertEqual(solution.exist([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCCED"),
+                         True)
+
+    def test_exist_v2(self):
+        solution = Solution()
+        self.assertEqual(solution.exist_v2([["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], "ABCCED"),
                          True)
