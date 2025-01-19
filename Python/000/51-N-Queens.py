1ef8717e5cb22cd7f533e326b83fd42eaf1cbd34
Atsushi <atu729@icloud.com>
2025-01-19 12:21:26 +0900

    add another solution

--- Python/000/51-N-Queens.py
+++ Python/000/51-N-Queens.py
@@ -37,6 +37,48 @@ class Solution:
         backtrack(0)
         return res
 
+    def solve_n_queens_v2(self, n: int) -> List[List[str]]:
+        res = []
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+
+            for c in range(n):
+                if self.is_safe(r, c, board):
+                    board[r][c] = 'Q'
+                    backtrack(r + 1)
+                    board[r][c] = "."
+
+        backtrack(0)
+        return res
+
+    def is_safe(self, r: int, c: int, board):
+        row = r - 1
+        while row >= 0:
+            if board[row][c] == "Q":
+                return False
+            row -= 1
+
+        row, col = r - 1, c - 1
+        while row >= 0 and col >= 0:
+            if board[row][col] == "Q":
+                return False
+            row -= 1
+            col -= 1
+
+        row, col = r - 1, c + 1
+        while row >= 0 and col < len(board):
+            if board[row][col] == "Q":
+                return False
+            row -= 1
+            col += 1
+
+        return True
+
 
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
@@ -44,6 +86,16 @@ class TestSolution(unittest.TestCase):
         solution = Solution()
         self.assertListEqual(
             solution.solve_n_queens(4),
+            [
+                [".Q..", "...Q", "Q...", "..Q."],
+                ["..Q.", "Q...", "...Q", ".Q.."]
+            ]
+        )
+
+    def test_solve_n_queens_v2(self):
+        solution = Solution()
+        self.assertListEqual(
+            solution.solve_n_queens_v2(4),
             [
                 [".Q..", "...Q", "Q...", "..Q."],
                 ["..Q.", "Q...", "...Q", ".Q.."]
