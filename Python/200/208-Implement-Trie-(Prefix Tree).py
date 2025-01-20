bf6de93ea7db1edd35b12c51b068418d41b95f0c
Atsushi <atu729@icloud.com>
2025-01-20 22:53:47 +0900

    add trie

---
+++ Python/200/208-Implement-Trie-(Prefix Tree).py
@@ -0,0 +1,48 @@
+import unittest
+
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.isEnd = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.isEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+
+#################### Test Case ####################
+class TestTrie(unittest.TestCase):
+    def test_trie(self):
+        trie = Trie()
+        trie.insert("apple")
+        self.assertEqual(trie.search("apple"), True)
+        self.assertEqual(trie.search("app"), False)
+        self.assertEqual(trie.startsWith("app"), True)
+        trie.insert("app")
+        self.assertEqual(trie.search("app"), True)
