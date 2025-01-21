9f538844a80b80ff7d68229899278f37c2b32b39
Atsushi <atu729@icloud.com>
2025-01-21 20:20:42 +0900

    add array approach

--- Python/200/208-Implement-Trie-(Prefix Tree).py
+++ Python/200/208-Implement-Trie-(Prefix Tree).py
@@ -7,6 +7,12 @@ class TrieNode:
         self.isEnd = False
 
 
+class TrieNodeV2:
+    def __init__(self):
+        self.children = [None] * 26
+        self.isEnd = False
+
+
 class Trie:
     def __init__(self):
         self.root = TrieNode()
@@ -36,10 +42,51 @@ class Trie:
         return True
 
 
+class TrieV2:
+    def __init__(self):
+        self.root = TrieNodeV2()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            index = ord(char) - ord("a")
+            if not node.children[index]:
+                node.children[index] = TrieNodeV2()
+            node = node.children[index]
+        node.isEnd = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            index = ord(char) - ord("a")
+            if not node.children[index]:
+                return False
+            node = node.children[index]
+        return node.isEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            index = ord(char) - ord("a")
+            if not node.children[index]:
+                return False
+            node = node.children[index]
+        return True
+
+
 #################### Test Case ####################
 class TestTrie(unittest.TestCase):
     def test_trie(self):
         trie = Trie()
+        trie.insert("apple")
+        self.assertEqual(trie.search("apple"), True)
+        self.assertEqual(trie.search("app"), False)
+        self.assertEqual(trie.startsWith("app"), True)
+        trie.insert("app")
+        self.assertEqual(trie.search("app"), True)
+
+    def test_trie_v2(self):
+        trie = TrieV2()
         trie.insert("apple")
         self.assertEqual(trie.search("apple"), True)
         self.assertEqual(trie.search("app"), False)
