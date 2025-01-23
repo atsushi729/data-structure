5d96df8b126fa2fb9dc88843daf625ca7ee6ddf2
Atsushi <atu729@icloud.com>
2025-01-23 20:08:39 +0900

    add another solution

--- Python/700/766-Toeplitz-Matix.py
+++ Python/700/766-Toeplitz-Matix.py
@@ -13,8 +13,22 @@ def is_toeplitz_matrix(matrix: list[list[int]]) -> bool:
     return True
 
 
+def is_toeplitz_matrix_v2(matrix: list[list[int]]) -> bool:
+    rows, column_stack = len(matrix), len(matrix[0])
+
+    for i in range(rows - 1):
+        for j in range(column_stack - 1):
+            if matrix[i][j] != matrix[i + 1][j + 1]:
+                return False
+    return True
+
+
 #################### Test Case ####################
 class TestIsToeplitzMatrix(unittest.TestCase):
     def test_is_toeplitz_matrix(self):
         self.assertEqual(is_toeplitz_matrix([[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]]), True)
         self.assertEqual(is_toeplitz_matrix([[1, 2], [2, 2]]), False)
+
+    def test_is_toeplitz_matrix_v2(self):
+        self.assertEqual(is_toeplitz_matrix_v2([[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]]), True)
+        self.assertEqual(is_toeplitz_matrix_v2([[1, 2], [2, 2]]), False)
