40ede55866917cba7131ac3901f1e1ddb432afbb
Atsushi <atu729@icloud.com>
2025-01-25 11:50:01 +0900

    add bfs approach

--- Python/200/200-Number-of-Islands.py
+++ Python/200/200-Number-of-Islands.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import List
 import unittest
 
@@ -29,6 +30,34 @@ class Solution:
 
         return is_land
 
+    def num_islands_v2(self, grid: List[List[str]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]  # Down, Up, Right, Left
+        rows, cols = len(grid), len(grid[0])
+        is_land = 0
+
+        def bfs(r, c):
+            grid[r][c] = "0"
+            q = deque()
+            q.append((r, c))
+
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    # Check if the current cell is out of bound or it is water
+                    if nr < 0 or nc < 0 or nr >= rows or nc >= cols or grid[nr][nc] == "0":
+                        continue
+                    q.append((nr, nc))
+                    grid[nr][nc] = "0"
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    bfs(r, c)
+                    is_land += 1
+
+        return is_land
+
 
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
@@ -41,6 +70,21 @@ class TestSolution(unittest.TestCase):
             ["0", "0", "0", "0", "0"]
         ]), 1)
         self.assertEqual(solution.num_islands([
+            ["1", "1", "0", "0", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "1", "0", "0"],
+            ["0", "0", "0", "1", "1"]
+        ]), 3)
+
+    def test_num_islands_v2(self):
+        solution = Solution()
+        self.assertEqual(solution.num_islands_v2([
+            ["1", "1", "1", "1", "0"],
+            ["1", "1", "0", "1", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "0", "0", "0"]
+        ]), 1)
+        self.assertEqual(solution.num_islands_v2([
             ["1", "1", "0", "0", "0"],
             ["1", "1", "0", "0", "0"],
             ["0", "0", "1", "0", "0"],
