039c1633564f7985b98997f7943ec40d46b4f7d3
Atsushi <atu729@icloud.com>
2025-01-29 20:09:40 +0900

    add bfs approach

--- Python/100/133-Clone-Graph.py
+++ Python/100/133-Clone-Graph.py
@@ -28,6 +28,24 @@ class Solution:
 
         return dfs(node) if node else None
 
+    def clone_graph_v2(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        oldToNew = {}
+        q = [node]
+        oldToNew[node] = Node(node.val)
+
+        while q:
+            cur = q.pop(0)
+            for nei in cur.neighbors:
+                if nei not in oldToNew:
+                    oldToNew[nei] = Node(nei.val)
+                    q.append(nei)
+                oldToNew[cur].neighbors.append(oldToNew[nei])
+
+        return oldToNew[node]
+
 
 class TestSolution(unittest.TestCase):
     def test_cloneGraph(self):
@@ -43,6 +61,27 @@ class TestSolution(unittest.TestCase):
         node4.neighbors = [node1, node3]
 
         copy1 = solution.cloneGraph(node1)
+        self.assertEqual(copy1.val, 1)
+        self.assertEqual(copy1.neighbors[0].val, 2)
+        self.assertEqual(copy1.neighbors[1].val, 4)
+        self.assertEqual(copy1.neighbors[0].neighbors[0].val, 1)
+        self.assertEqual(copy1.neighbors[0].neighbors[1].val, 3)
+        self.assertEqual(copy1.neighbors[1].neighbors[0].val, 1)
+        self.assertEqual(copy1.neighbors[1].neighbors[1].val, 3)
+
+    def test_clone_graph_v2(self):
+        solution = Solution()
+
+        node1 = Node(1)
+        node2 = Node(2)
+        node3 = Node(3)
+        node4 = Node(4)
+        node1.neighbors = [node2, node4]
+        node2.neighbors = [node1, node3]
+        node3.neighbors = [node2, node4]
+        node4.neighbors = [node1, node3]
+
+        copy1 = solution.clone_graph_v2(node1)
         self.assertEqual(copy1.val, 1)
         self.assertEqual(copy1.neighbors[0].val, 2)
         self.assertEqual(copy1.neighbors[1].val, 4)
