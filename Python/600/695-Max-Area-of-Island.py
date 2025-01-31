e6b0f316ab24e9c510874f160c941bceae3d8ba0
Atsushi <atu729@icloud.com>
2025-01-31 21:31:48 +0900

    add bfs approach

--- Python/600/695-Max-Area-of-Island.py
+++ Python/600/695-Max-Area-of-Island.py
@@ -1,5 +1,6 @@
 from typing import List
 import unittest
+from collections import deque
 
 
 #################### Solution ####################
@@ -32,6 +33,40 @@ class Solution:
             for c in range(cols):
                 if grid[r][c] == 1:
                     current_area = dfs(r, c)
+                    max_area = max(max_area, current_area)
+
+        return max_area
+
+    def max_area_of_island_v2(self, grid: List[List[int]]) -> int:
+        """"
+        Time Complexity: O(m * n)
+        Space Complexity: O(m * n)
+        """
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        rows, cols = len(grid), len(grid[0])
+        max_area = 0
+
+        def bfs(r, c):
+            q = deque()
+            q.append((r, c))
+            grid[r][c] = 0
+            area = 1
+
+            while q:
+                row, col = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = row + dr, col + dc
+                    if nr < 0 or nc < 0 or nr >= rows or nc >= cols or grid[nr][nc] == 0:
+                        continue
+                    q.append((nr, nc))
+                    grid[nr][nc] = 0
+                    area += 1
+            return area
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    current_area = bfs(r, c)
                     max_area = max(max_area, current_area)
 
         return max_area
@@ -107,4 +142,44 @@ class TestSolution(unittest.TestCase):
             [0, 0, 1, 0, 0, 0, 0, 0],
             [0, 1, 0, 0, 0, 0, 0, 0],
             [1, 0, 0, 0, 0, 0, 0, 0]
+        ]), 1)
+
+    def test_max_area_of_island_v2(self):
+        self.assertEqual(Solution().max_area_of_island_v2([
+            [1, 1, 0, 0, 0],
+            [1, 1, 0, 0, 0],
+            [0, 0, 0, 1, 1],
+            [0, 0, 0, 1, 1]
+        ]), 4)
+        self.assertEqual(Solution().max_area_of_island_v2([
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ]), 0)
+        self.assertEqual(Solution().max_area_of_island_v2([
+            [1, 1, 0, 1, 1],
+            [1, 0, 0, 0, 0],
+            [1, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1]
+        ]), 10)
+        self.assertEqual(Solution().max_area_of_island_v2([
+            [1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1]
+        ]), 14)
+        self.assertEqual(Solution().max_area_of_island_v2([
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
         ]), 1)
