c9c5d96bf8189d66543eb905931172a268be6d12
Atsushi <atu729@icloud.com>
2025-02-09 23:25:50 +0900

    add Number of Connected Components in an Undirected Graph

---
+++ Python/300/323-Number-of-Connected-Components-in-an-Undirected-Graph.py
@@ -0,0 +1,37 @@
+from typing import List
+import unittest
+
+
+class Solution:
+    def count_components(self, n: int, edges: List[List[int]]) -> int:
+        adj = [[] for _ in range(n)]
+        visit = [False] * n
+
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        def dfs(node):
+            for nei in adj[node]:
+                if not visit[nei]:
+                    visit[nei] = True
+                    dfs(nei)
+
+        res = 0
+        for node in range(n):
+            if not visit[node]:
+                visit[node] = True
+                dfs(node)
+                res += 1
+        return res
+
+
+class TestSolution(unittest.TestCase):
+    def test_countComponents(self):
+        solution = Solution()
+        self.assertEqual(solution.count_components(5, [[0, 1], [1, 2], [3, 4]]), 2)
+        self.assertEqual(solution.count_components(5, [[0, 1], [1, 2], [2, 3], [3, 4]]), 1)
+        self.assertEqual(solution.count_components(5, [[0, 1], [1, 2], [2, 3], [3, 4], [0, 4]]), 1)
+        self.assertEqual(solution.count_components(5, [[0, 1], [1, 2], [2, 3], [3, 4], [0, 4], [2, 4]]), 1)
+        self.assertEqual(solution.count_components(5, [[0, 1], [1, 2], [2, 3], [3, 4], [0, 4], [2, 4], [0, 2]]), 1)
+
