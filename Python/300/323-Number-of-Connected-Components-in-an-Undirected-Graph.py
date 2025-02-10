a21d8bb2a9e15c6e3b600ce0118a9814b0c2754c
Atsushi <atu729@icloud.com>
2025-02-10 23:29:58 +0900

    add another solution

--- Python/300/323-Number-of-Connected-Components-in-an-Undirected-Graph.py
+++ Python/300/323-Number-of-Connected-Components-in-an-Undirected-Graph.py
@@ -1,5 +1,6 @@
 from typing import List
 import unittest
+from collections import deque
 
 
 class Solution:
@@ -25,6 +26,30 @@ class Solution:
                 res += 1
         return res
 
+    def count_components_v2(self, n: int, edges: List[List[int]]) -> int:
+        adj = [[] for _ in range(n)]
+        visit = [False] * n
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        def bfs(node):
+            q = deque([node])
+            visit[node] = True
+            while q:
+                cur = q.popleft()
+                for nei in adj[cur]:
+                    if not visit[nei]:
+                        visit[nei] = True
+                        q.append(nei)
+
+        res = 0
+        for node in range(n):
+            if not visit[node]:
+                bfs(node)
+                res += 1
+        return res
+
 
 class TestSolution(unittest.TestCase):
     def test_countComponents(self):
@@ -35,3 +60,10 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(solution.count_components(5, [[0, 1], [1, 2], [2, 3], [3, 4], [0, 4], [2, 4]]), 1)
         self.assertEqual(solution.count_components(5, [[0, 1], [1, 2], [2, 3], [3, 4], [0, 4], [2, 4], [0, 2]]), 1)
 
+    def test_count_components_v2(self):
+        solution = Solution()
+        self.assertEqual(solution.count_components_v2(5, [[0, 1], [1, 2], [3, 4]]), 2)
+        self.assertEqual(solution.count_components_v2(5, [[0, 1], [1, 2], [2, 3], [3, 4]]), 1)
+        self.assertEqual(solution.count_components_v2(5, [[0, 1], [1, 2], [2, 3], [3, 4], [0, 4]]), 1)
+        self.assertEqual(solution.count_components_v2(5, [[0, 1], [1, 2], [2, 3], [3, 4], [0, 4], [2, 4]]), 1)
+        self.assertEqual(solution.count_components_v2(5, [[0, 1], [1, 2], [2, 3], [3, 4], [0, 4], [2, 4], [0, 2]]), 1)
