0fdbfbed5e2782aef71fed0238c987bce51f3d80
Atsushi <atu729@icloud.com>
2025-02-18 20:18:22 +0900

    add recursion

--- Python/700/746-Min-Cost-Climbing-Stairs.py
+++ Python/700/746-Min-Cost-Climbing-Stairs.py
@@ -33,6 +33,18 @@ class Solution:
         return min(dfs(0), dfs(1))
 
 
+    def min_cost_climbing_stairs_v3(self, cost: List[int]) -> int:
+        """
+        Time Complexity: O(n^2)
+        Space Complexity: O(n)
+        """
+        def dfs(i):
+            if i >= len(cost):
+                return 0
+            return cost[i] + min(dfs(i + 1), dfs(i + 2))
+
+        return min(dfs(0), dfs(1))
+
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
     def test_min_cost_climbing_stairs(self):
@@ -44,3 +56,8 @@ class TestSolution(unittest.TestCase):
         solution = Solution()
         self.assertEqual(solution.min_cost_climbing_stairs_v2([10, 15, 20]), 15)
         self.assertEqual(solution.min_cost_climbing_stairs_v2([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]), 6)
+
+    def test_min_cost_climbing_stairs_v3(self):
+        solution = Solution()
+        self.assertEqual(solution.min_cost_climbing_stairs_v3([10, 15, 20]), 15)
+        self.assertEqual(solution.min_cost_climbing_stairs_v3([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]), 6)
