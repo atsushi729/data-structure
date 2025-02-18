9f93b35c9368db2100ce172413f3e622a9c88380
Atsushi <atu729@icloud.com>
2025-02-18 19:40:02 +0900

    add top down

--- Python/700/746-Min-Cost-Climbing-Stairs.py
+++ Python/700/746-Min-Cost-Climbing-Stairs.py
@@ -13,6 +13,25 @@ class Solution:
             cost[i] += min(cost[i + 1], cost[i + 2])
         return min(cost[0], cost[1])
 
+    def min_cost_climbing_stairs_v2(self, cost: List[int]) -> int:
+        """
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        """
+        cache = [-1] * len(cost)
+
+        def dfs(i):
+            if i >= len(cost):
+                return 0
+
+            if cache[i] != -1:
+                return cache[i]
+
+            cache[i] = cost[i] + min(dfs(i + 1), dfs(i + 2))
+            return cache[i]
+
+        return min(dfs(0), dfs(1))
+
 
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
@@ -20,3 +39,8 @@ class TestSolution(unittest.TestCase):
         solution = Solution()
         self.assertEqual(solution.min_cost_climbing_stairs([10, 15, 20]), 15)
         self.assertEqual(solution.min_cost_climbing_stairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]), 6)
+
+    def test_min_cost_climbing_stairs_v2(self):
+        solution = Solution()
+        self.assertEqual(solution.min_cost_climbing_stairs_v2([10, 15, 20]), 15)
+        self.assertEqual(solution.min_cost_climbing_stairs_v2([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]), 6)
