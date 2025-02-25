69b027dcc8f2a89b1a45b2896524031a1e71f9bb
Atsushi <atu729@icloud.com>
2025-02-25 20:56:10 +0900

    add dp approach

--- Python/600/647-Palindromic-Substrings.py
+++ Python/600/647-Palindromic-Substrings.py
@@ -21,6 +21,22 @@ class Solution:
             r += 1
         return count
 
+    def count_substrings_v2(self, s: str) -> int:
+        """
+        Time Complexity: O(n^2)
+        Space Complexity: O(n^2)
+        """
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        count = 0
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+                    count += 1
+        return count
+
 
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
@@ -33,3 +49,9 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.solution.count_substrings('aaa'), 6)
         self.assertEqual(self.solution.count_substrings('abccba'), 9)
         self.assertEqual(self.solution.count_substrings('abccbaa'), 11)
+
+    def test_countSubstrings_v2(self):
+        self.assertEqual(self.solution.count_substrings_v2('abc'), 3)
+        self.assertEqual(self.solution.count_substrings_v2('aaa'), 6)
+        self.assertEqual(self.solution.count_substrings_v2('abccba'), 9)
+        self.assertEqual(self.solution.count_substrings_v2('abccbaa'), 11)
