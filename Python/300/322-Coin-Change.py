ebdfcac85c5ded48c3df720d82a9489780d31d9b
Atsushi <atu729@icloud.com>
2025-03-06 23:48:10 +0900

    add recursion

--- Python/300/322-Coin-Change.py
+++ Python/300/322-Coin-Change.py
@@ -4,6 +4,10 @@ import unittest
 
 class Solution:
     def coin_change(self, coins: List[int], amount: int) -> int:
+        """
+        Time Complexity: O(n*m)
+        Space Complexity: O(m)
+        """
         memo = {}
 
         def dfs(amount):
@@ -25,6 +29,29 @@ class Solution:
         min_coins = dfs(amount)
         return -1 if min_coins >= 1e9 else min_coins
 
+    def coin_change_v2(self, coins: List[int], amount: int) -> int:
+        """
+        Time Complexity: O(n^m)
+        Space Complexity: O(1)
+        """
+        def dfs(amount):
+            if amount == 0:
+                return 0
+
+            res = 1e9
+
+            for coin in coins:
+                if amount - coin >= 0:
+                    res = min(res, 1 + dfs(amount - coin))
+
+            return res
+
+        min_coins = dfs(amount)
+        if min_coins >= 1e9:
+            return -1
+        return min_coins
+
+
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -37,3 +64,10 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.sol.coin_change([1], 0), 0)
         self.assertEqual(self.sol.coin_change([1], 1), 1)
         self.assertEqual(self.sol.coin_change([1], 2), 2)
+
+    def test_coin_change_v2(self):
+        self.assertEqual(self.sol.coin_change_v2([1, 2, 5], 11), 3)
+        self.assertEqual(self.sol.coin_change_v2([2], 3), -1)
+        self.assertEqual(self.sol.coin_change_v2([1], 0), 0)
+        self.assertEqual(self.sol.coin_change_v2([1], 1), 1)
+        self.assertEqual(self.sol.coin_change_v2([1], 2), 2)
