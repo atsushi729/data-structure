46f5acc61b32420222c00f545906eea4a0b97d36
Atsushi <atu729@icloud.com>
2025-03-10 23:49:55 +0900

    add two pointer

--- Python/100/152-Maximum-Product-Subarray.py
+++ Python/100/152-Maximum-Product-Subarray.py
@@ -15,6 +15,44 @@ class Solution:
 
         return res
 
+    def max_product_v2(self, nums: List[int]) -> int:
+        A = []
+        cur = []
+        res = float('-inf')
+
+        for num in nums:
+            res = max(res, num)
+            if num == 0:
+                if cur:
+                    A.append(cur)
+                cur = []
+            else:
+                cur.append(num)
+
+        if cur:
+            A.append(cur)
+
+        for sub in A:
+            negs = sum(1 for i in sub if i < 0)
+            prod = 1
+            need = negs if negs % 2 == 0 else negs - 1
+            negs = 0
+            j = 0
+
+            for i in range(len(sub)):
+                prod *= sub[i]
+                if sub[i] < 0:
+                    negs += 1
+                    while negs > need:
+                        prod //= sub[j]
+                        if sub[j] < 0:
+                            negs -= 1
+                        j += 1
+                if j <= i:
+                    res = max(res, prod)
+
+        return res
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -27,3 +65,10 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.sol.max_product([-2]), -2)
         self.assertEqual(self.sol.max_product([0, 2]), 2)
         self.assertEqual(self.sol.max_product([0, 2, 0]), 2)
+
+    def test_maxProduct_v2(self):
+        self.assertEqual(self.sol.max_product_v2([2, 3, -2, 4]), 6)
+        self.assertEqual(self.sol.max_product_v2([-2, 0, -1]), 0)
+        self.assertEqual(self.sol.max_product_v2([-2]), -2)
+        self.assertEqual(self.sol.max_product_v2([0, 2]), 2)
+        self.assertEqual(self.sol.max_product_v2([0, 2, 0]), 2)
