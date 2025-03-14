bf9934816e6653213ce6a99815090356c04fde8f
Atsushi <atu729@icloud.com>
2025-03-14 22:12:33 +0900

    add hash set approach

--- Python/100/139-Word-Break.py
+++ Python/100/139-Word-Break.py
@@ -19,6 +19,20 @@ class Solution:
 
         return dfs(0)
 
+    def wordBreak_v2(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+
+        def dfs(i):
+            if i == len(s):
+                return True
+            for j in range(i, len(s)):
+                if s[i: j + 1] in word_set:
+                    if dfs(j + 1):
+                        return True
+            return False
+
+        return dfs(0)
+
 
 class TestSolution(unittest.TestCase):
 
@@ -30,3 +44,8 @@ class TestSolution(unittest.TestCase):
         self.assertTrue(self.sol.wordBreak("leetcode", ["leet", "code"]))
         self.assertTrue(self.sol.wordBreak("applepenapple", ["apple", "pen"]))
         self.assertFalse(self.sol.wordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]))
+
+    def test_wordBreak_v2(self):
+        self.assertTrue(self.sol.wordBreak_v2("leetcode", ["leet", "code"]))
+        self.assertTrue(self.sol.wordBreak_v2("applepenapple", ["apple", "pen"]))
+        self.assertFalse(self.sol.wordBreak_v2("catsandog", ["cats", "dog", "sand", "and", "cat"]))
