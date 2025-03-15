26edcc992734ddc596438fd78c9de2852827d861
Atsushi <atu729@icloud.com>
2025-03-15 22:10:14 +0900

    add bottom up approach

--- Python/100/139-Word-Break.py
+++ Python/100/139-Word-Break.py
@@ -33,6 +33,19 @@ class Solution:
 
         return dfs(0)
 
+    def wordBreak_v3(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j: i] in word_set:
+                    dp[i] = True
+                    break
+
+        return dp[-1]
+
 
 class TestSolution(unittest.TestCase):
 
@@ -49,3 +62,8 @@ class TestSolution(unittest.TestCase):
         self.assertTrue(self.sol.wordBreak_v2("leetcode", ["leet", "code"]))
         self.assertTrue(self.sol.wordBreak_v2("applepenapple", ["apple", "pen"]))
         self.assertFalse(self.sol.wordBreak_v2("catsandog", ["cats", "dog", "sand", "and", "cat"]))
+
+    def test_wordBreak_v3(self):
+        self.assertTrue(self.sol.wordBreak_v3("leetcode", ["leet", "code"]))
+        self.assertTrue(self.sol.wordBreak_v3("applepenapple", ["apple", "pen"]))
+        self.assertFalse(self.sol.wordBreak_v3("catsandog", ["cats", "dog", "sand", "and", "cat"]))
