22e2459672a263d4e9c559fdf99970f93a2a708c
Atsushi <atu729@icloud.com>
2025-03-22 23:41:54 +0900

    add top down

--- Python/400/416-Partition-Equal-Subset-Sum.py
+++ Python/400/416-Partition-Equal-Subset-Sum.py
@@ -17,6 +17,27 @@ class Solution:
 
         return dfs(0, sum(nums) // 2)
 
+    def can_partition_v2(self, nums: List[int]) -> bool:
+        total = sum(nums)
+        if total % 2 != 0:
+            return False
+
+        target = total // 2
+        n = len(nums)
+        memo = [[-1] * (target + 1) for _ in range(n + 1)]
+
+        def dfs(i, target):
+            if target == 0:
+                return True
+            if i >= n or target < 0:
+                return False
+            if memo[i][target] != -1:
+                return memo[i][target]
+            memo[i][target] = (dfs(i + 1, target) or dfs(i + 1, target - nums[i]))
+            return memo[i][target]
+
+        return dfs(0, target)
+
 
 class TestSolution(unittest.TestCase):
 
@@ -27,3 +48,7 @@ class TestSolution(unittest.TestCase):
     def test_can_partition(self):
         self.assertTrue(self.solution.can_partition([1, 5, 11, 5]))
         self.assertFalse(self.solution.can_partition([1, 2, 3, 5]))
+
+    def test_can_partition_v2(self):
+        self.assertTrue(self.solution.can_partition_v2([1, 5, 11, 5]))
+        self.assertFalse(self.solution.can_partition_v2([1, 2, 3, 5]))
