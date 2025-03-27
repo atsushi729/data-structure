ccc525ab5fa0649382baea08657bbc70ce8515c9
Atsushi <atu729@icloud.com>
2025-03-27 23:50:17 +0900

    add bottom up

--- Python/000/062-Unique-Paths.py
+++ Python/000/062-Unique-Paths.py
@@ -28,6 +28,16 @@ class Solution:
 
         return dfs(0, 0)
 
+    def uniquePaths_v3(self, m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp[m - 1][n - 1] = 1
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                dp[i][j] += dp[i + 1][j] + dp[i][j + 1]
+
+        return dp[0][0]
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -57,3 +67,15 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.s.uniquePaths_v2(2, 2), 2)
         self.assertEqual(self.s.uniquePaths_v2(2, 3), 3)
         self.assertEqual(self.s.uniquePaths_v2(3, 1), 1)
+
+    def test_unique_paths_v3(self):
+        self.assertEqual(self.s.uniquePaths_v3(3, 7), 28)
+        self.assertEqual(self.s.uniquePaths_v3(3, 2), 3)
+        self.assertEqual(self.s.uniquePaths_v3(7, 3), 28)
+        self.assertEqual(self.s.uniquePaths_v3(3, 3), 6)
+        self.assertEqual(self.s.uniquePaths_v3(1, 1), 1)
+        self.assertEqual(self.s.uniquePaths_v3(1, 2), 1)
+        self.assertEqual(self.s.uniquePaths_v3(2, 1), 1)
+        self.assertEqual(self.s.uniquePaths_v3(2, 2), 2)
+        self.assertEqual(self.s.uniquePaths_v3(2, 3), 3)
+        self.assertEqual(self.s.uniquePaths_v3(3, 1), 1)
