223bcf077f41218a486fda67e2b76bba2baecb6c
Atsushi <atu729@icloud.com>
2025-03-31 23:42:50 +0900

    add another solution

--- Python/000/001-Tow-Sum.py
+++ Python/000/001-Tow-Sum.py
@@ -14,9 +14,28 @@ def two_sum(nums: [int], target: int) -> list[int]:
     return []
 
 
+def two_sum_v2(nums: [int], target: int) -> list[int]:
+    left, right = 0, len(nums) - 1
+
+    for i in range(len(nums)):
+        if nums[left] + nums[right] == target:
+            return [left, right]
+        elif nums[left] + nums[right] < target:
+            left += 1
+        else:
+            right -= 1
+
+    return []
+
+
 #################### Test Case ####################
 class TestTowSum(unittest.TestCase):
     def test_two_sum(self):
         self.assertEqual(two_sum([2, 7, 11, 15], 9), [0, 1])
         self.assertEqual(two_sum([3, 2, 4], 6), [1, 2])
         self.assertEqual(two_sum([3, 3], 6), [0, 1])
+
+    def test_two_sum_v2(self):
+        self.assertEqual(two_sum_v2([2, 7, 11, 15], 9), [0, 1])
+        self.assertEqual(two_sum_v2([2, 3, 4], 6), [0, 2])
+        self.assertEqual(two_sum_v2([3, 3], 6), [0, 1])
