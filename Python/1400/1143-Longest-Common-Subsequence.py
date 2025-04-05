60e4852a6df85c35f97d727bfc17d5f5d65980ac
Atsushi <atu729@icloud.com>
2025-04-05 23:31:15 +0900

    add bottom up

--- Python/1400/1143-Longest-Common-Subsequence.py
+++ Python/1400/1143-Longest-Common-Subsequence.py
@@ -31,6 +31,17 @@ class Solution:
 
         return dfs(0, 0)
 
+    def longest_common_subsequence_v3(self, text1: str, text2: str) -> int:
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+        return dp[0][0]
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -50,3 +61,10 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.solution.longest_common_subsequence_v2("abc", "def"), 0)
         self.assertEqual(self.solution.longest_common_subsequence_v2("", ""), 0)
         self.assertEqual(self.solution.longest_common_subsequence_v2("a", "a"), 1)
+
+    def test_longest_common_subsequence_v3(self):
+        self.assertEqual(self.solution.longest_common_subsequence_v3("abcde", "ace"), 3)
+        self.assertEqual(self.solution.longest_common_subsequence_v3("abc", "abc"), 3)
+        self.assertEqual(self.solution.longest_common_subsequence_v3("abc", "def"), 0)
+        self.assertEqual(self.solution.longest_common_subsequence_v3("", ""), 0)
+        self.assertEqual(self.solution.longest_common_subsequence_v3("a", "a"), 1)
