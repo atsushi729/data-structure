a2761703998df59a834bebc988a3a1365e27fa3a
Atsushi <atu729@icloud.com>
2025-04-09 22:57:46 +0900

    add top down

--- Python/300/309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.py
+++ Python/300/309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.py
@@ -19,6 +19,26 @@ class Solution:
 
         return dfs(0, True)
 
+    def max_profit_v2(self, prices: List[int]) -> int:
+        dp = {}  # key=(i, buying) val=max_profit
+
+        def dfs(i, buying):
+            if i >= len(prices):
+                return 0
+            if (i, buying) in dp:
+                return dp[(i, buying)]
+
+            cooldown = dfs(i + 1, buying)
+            if buying:
+                buy = dfs(i + 1, not buying) - prices[i]
+                dp[(i, buying)] = max(buy, cooldown)
+            else:
+                sell = dfs(i + 2, not buying) + prices[i]
+                dp[(i, buying)] = max(sell, cooldown)
+            return dp[(i, buying)]
+
+        return dfs(0, True)
+
 
 class TestSolution(unittest.TestCase):
 
@@ -32,3 +52,10 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(self.solution.max_profit([1, 2]), 1)
         self.assertEqual(self.solution.max_profit([2, 1]), 0)
         self.assertEqual(self.solution.max_profit([1, 2, 3]), 2)
+
+    def test_max_profit_v2(self):
+        self.assertEqual(self.solution.max_profit_v2([1, 2, 3, 0, 2]), 3)
+        self.assertEqual(self.solution.max_profit_v2([1]), 0)
+        self.assertEqual(self.solution.max_profit_v2([1, 2]), 1)
+        self.assertEqual(self.solution.max_profit_v2([2, 1]), 0)
+        self.assertEqual(self.solution.max_profit_v2([1, 2, 3]), 2)
