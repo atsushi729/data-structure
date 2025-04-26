3441b69eb2185cd5e45e93d32708cf6b41118a57
Atsushi <atu729@icloud.com>
2025-04-26 14:58:00 +0900

    add top_k_frequent_v2 method using Counter for frequency counting and corresponding test cases

--- Python/300/347-top-k-frequent-elements.py
+++ Python/300/347-top-k-frequent-elements.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import Counter
 
 
 #################### Solution ####################
@@ -23,10 +24,30 @@ def top_k_frequent(nums: [int], k: int) -> [int]:
     return top_k_frequents
 
 
+def top_k_frequent_v2(nums: [int], k: int) -> [int]:
+    # Count frequency of nums.
+    # e.g. {1 : 3, 2 : 2, 3 : 1}
+    frequent_counter = Counter(nums)
+
+    # Sorted based on value.
+    # [(3, 1), (2, 2), (1, 3)]
+    sorted_frequent_counter = sorted(frequent_counter.items(), key=lambda x: x[1], reverse=True)
+
+    # Create key which contain top k frequents value.
+    top_k_frequents = [item[0] for item in sorted_frequent_counter[:k]]
+
+    return top_k_frequents
+
+
 #################### Test Case ####################
 class TestTopKFrequent(unittest.TestCase):
     def test_top_k_frequent(self):
         self.assertEqual(top_k_frequent([1, 1, 1, 2, 2, 3], 2), [1, 2])
+        self.assertEqual(top_k_frequent([1, 2], 2), [1, 2])
+
+    def test_top_k_frequent_v2(self):
+        self.assertEqual(top_k_frequent_v2([1, 1, 1, 2, 2, 3], 2), [1, 2])
+        self.assertEqual(top_k_frequent_v2([1, 2], 2), [1, 2])
 
 
 #################### Test via terminal ####################
