53b4d822a280ed82450c9b0c01b35ea31bfac394
Atsushi <atu729@icloud.com>
2025-04-29 20:57:24 +0900

    add max_distinct_elements method with unit tests for maximum number of distinct elements after operations

---
+++ Python/other/max-profit.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+from typing import List
+import unittest
+
+
+class Solution:
+    def countTwoSumPairs(self, nums: List[int], target: int) -> int:
+        count = 0
+        num_map = defaultdict(int)
+
+        for num in nums:
+            complement = target - num
+            if num_map[complement] > 0:
+                count += 1
+                num_map[complement] -= 1
+            else:
+                num_map[num] += 1
+
+        return count
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+        self.test_cases = [
+            ([1, 2, 3, 4], 5, 2),
+            ([1, 1, 2, 2], 3, 2),
+            ([1, 2, 3, 4], 6, 1),
+            ([0, -1, 1], 0, 1),
+            ([5, -5, 5], 0, 1),
+            ([], 0, 0),
+            ([1], 2, 0),
+        ]
+
+    def test_count_two_sum_pairs(self):
+        for nums, target, expected in self.test_cases:
+            result = self.solution.countTwoSumPairs(nums, target)
+            self.assertEqual(result, expected)
