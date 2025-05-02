2caa0f3a854c1338131f99ddcf251b620ad714e2
Atsushi <atu729@icloud.com>
2025-05-02 09:46:23 +0900

    add count_binary_substrings_v2 method with unit tests for binary substring counting

--- Python/600/696-Count-Binary-Substrings.py
+++ Python/600/696-Count-Binary-Substrings.py
@@ -14,6 +14,22 @@ class Solution:
         groups.append(count)
         return sum(min(groups[i], groups[i + 1]) for i in range(len(groups) - 1))
 
+    def count_binary_substrings_v2(self, s: str) -> int:
+        n = len(s)
+        prev, cur = 0, 1
+        output = 0
+
+        for i in range(1, n):
+            if s[i] != s[i - 1]:
+                output += min(cur, prev)
+                prev = cur
+                cur = 1
+            else:
+                cur += 1
+
+        output += min(prev, cur)
+        return output
+
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
@@ -29,4 +45,9 @@ class TestSolution(unittest.TestCase):
     def test_count_binary_substrings(self):
         for s, expected in self.test_case:
             result = self.solution.count_binary_substrings(s)
+            self.assertEqual(result, expected)
+
+    def test_count_binary_substrings_v2(self):
+        for s, expected in self.test_case:
+            result = self.solution.count_binary_substrings_v2(s)
             self.assertEqual(result, expected)
