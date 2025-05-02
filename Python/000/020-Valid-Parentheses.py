b4b8121f2e6ffe4ce657659d7be19dd23ecaba70
Atsushi <atu729@icloud.com>
2025-05-02 09:59:06 +0900

    fix variable name typo in 020-Valid-Parentheses.py for consistency

--- Python/000/020-Valid-Parentheses.py
+++ Python/000/020-Valid-Parentheses.py
@@ -6,20 +6,20 @@ def is_valid(s: str) -> bool:
     stack = []
     pair_lists = {")": "(", "]": "[", "}": "{"}
 
-    for simbol in s:
-        ## simbol = ")" 閉じるの場合
-        if simbol in pair_lists:
+    for symbol in s:
+        ## symbol = ")" 閉じるの場合
+        if symbol in pair_lists:
             """
             以下のケースはエラーとする。
             Case1 : もし閉じ括弧が入ってきたのにも関わらず、stackが空
-            Case2 : 直前の配列の記号がsimbolの開始シンボルと異なる
+            Case2 : 直前の配列の記号がsymbolの開始シンボルと異なる
             """
-            if not stack or stack[-1] != pair_lists[simbol]:
+            if not stack or stack[-1] != pair_lists[symbol]:
                 return False
             stack.pop()
-        ## simbol = "(" 開始の場合
+        ## symbol = "(" 開始の場合
         else:
-            stack.append(simbol)
+            stack.append(symbol)
 
     ## ペアが正しく定義されている場合、比率は1:1となり、からなずstackは空になる。
     return len(stack) == 0
