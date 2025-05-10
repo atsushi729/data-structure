af64f262fd96e9b1b750d67f3de413c08408a07c
Atsushi <atu729@icloud.com>
2025-05-10 11:03:57 +0900

    add singleNumber_v2 method with unit tests for an alternative approach to finding the single number in a list

--- Python/100/136-Single-Number.py
+++ Python/100/136-Single-Number.py
@@ -14,6 +14,19 @@ class Solution:
             answer = answer ^ num
         return answer
 
+    def singleNumber_v2(self, nums: List[int]) -> int:
+        """
+        Time complexity: O(n)
+        Space complexity: O(n)
+        """
+        seen = set()
+        for num in nums:
+            if num in seen:
+                seen.remove(num)
+            else:
+                seen.add(num)
+        return list(seen)[0]
+
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
@@ -22,11 +35,16 @@ class TestSolution(unittest.TestCase):
             ([2, 2, 1], 1),
             ([4, 1, 2, 1, 2], 4),
             ([1], 1),
-            ([0, 0, 0, 1], 1),
-            ([5, 5, 5], 5),
+            ([0, 0, 1], 1),
+            ([5, 5, 6], 6),
         ]
 
     def test_single_number(self):
         for nums, expected in self.test_cases:
             result = self.solution.singleNumber(nums)
+            self.assertEqual(result, expected)
+
+    def test_single_number_v2(self):
+        for nums, expected in self.test_cases:
+            result = self.solution.singleNumber_v2(nums)
             self.assertEqual(result, expected)
