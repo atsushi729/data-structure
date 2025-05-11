6bdf43463ddbc2ebd42cda1373d16f4fff99cba2
Atsushi <atu729@icloud.com>
2025-05-11 11:40:21 +0900

    add count_bits methods with unit tests for counting the number of 1 bits in integers

---
+++ Python/300/338-Counting-Bits.py
@@ -0,0 +1,52 @@
+from typing import List
+import unittest
+
+
+class Solution:
+    def count_bits(self, n: int) -> List[int]:
+        res = []
+        for num in range(n + 1):
+            one = 0
+            for i in range(32):
+                if num & (1 << i):
+                    one += 1
+            res.append(one)
+        return res
+
+    def count_bits_v2(self, n: int) -> List[int]:
+        ans_list = []
+
+        for i in range(n + 1):
+            res = self.count_bit(i)
+            ans_list.append(res)
+        return ans_list
+
+    def count_bit(self, nums: int):
+        res = 0
+
+        while nums:
+            res += nums % 2
+            nums = nums >> 1
+        return res
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+        self.test_cases = [
+            (2, [0, 1, 1]),
+            (5, [0, 1, 1, 2, 1, 2]),
+            (0, [0]),
+            (7, [0, 1, 1, 2, 1, 2, 2, 3]),
+            (10, [0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2]),
+        ]
+
+    def test_count_bits(self):
+        for n, expected in self.test_cases:
+            result = self.solution.count_bits(n)
+            self.assertEqual(result, expected)
+
+    def test_count_bits_v2(self):
+        for n, expected in self.test_cases:
+            result = self.solution.count_bits_v2(n)
+            self.assertEqual(result, expected)
