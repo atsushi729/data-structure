9c0b2f1430d2003b6b8997382ce8e33c9ce83483
Atsushi <atu729@icloud.com>
2025-05-17 22:31:01 +0900

    add reverse_bits_v2 method with unit tests for validating bit reversal functionality

--- Python/100/190-Reverse-Bits.py
+++ Python/100/190-Reverse-Bits.py
@@ -9,6 +9,22 @@ class Solution:
             res += (bit << (31 - i))
         return res
 
+    def reverse_bits_v2(self, n: int) -> int:
+        binary = ""
+
+        for i in range(32):
+            if n & (1 << i):
+                binary += "1"
+            else:
+                binary += "0"
+
+        res = 0
+
+        for i, bit in enumerate(binary[::-1]):
+            if bit == "1":
+                res |= (1 << i)
+        return res
+
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
@@ -25,4 +41,9 @@ class TestSolution(unittest.TestCase):
     def test_reverse_bits(self):
         for n, expected in self.test_cases:
             result = self.solution.reverse_bits(n)
+            self.assertEqual(result, expected)
+
+    def test_reverse_bits_v2(self):
+        for n, expected in self.test_cases:
+            result = self.solution.reverse_bits_v2(n)
             self.assertEqual(result, expected)
