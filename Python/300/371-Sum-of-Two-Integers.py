0be3d258ef24dfd50ba3b607d3435d8cd42d9989
Atsushi <atu729@icloud.com>
2025-05-28 22:52:47 +0900

    add get_sum_v2 method with unit tests for calculating the sum of two integers using bit manipulation

--- Python/300/371-Sum-of-Two-Integers.py
+++ Python/300/371-Sum-of-Two-Integers.py
@@ -12,6 +12,25 @@ class Solution:
             b = carry & mask
         return a if a <= max_int else ~(a ^ mask)
 
+    def get_sum_v2(self, a: int, b: int) -> int:
+        carry = 0
+        res = 0
+        mask = 0xFFFFFFFF
+
+        for i in range(32):
+            a_bit = (a >> i) & 1
+            b_bit = (b >> i) & 1
+
+            cur_bit = a_bit ^ b_bit ^ carry
+            carry = a_bit + b_bit + carry > 1
+
+            if cur_bit:
+                res |= (1 << i)
+
+        if res > 0x7FFFFFFF:
+            res = ~(res ^ mask)
+        return res
+
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
@@ -28,4 +47,9 @@ class TestSolution(unittest.TestCase):
     def test_get_sum(self):
         for a, b, expected in self.test_cases:
             result = self.solution.get_sum(a, b)
+            self.assertEqual(result, expected)
+
+    def test_get_sum_v2(self):
+        for a, b, expected in self.test_cases:
+            result = self.solution.get_sum_v2(a, b)
             self.assertEqual(result, expected)
