b488e086b00e6c03b494b1a3235266a9e19b8b36
Atsushi <atu729@icloud.com>
2025-05-31 20:21:02 +0900

    add reverse_v4 method with unit tests for reversing an integer using string manipulation

--- Python/000/007-Reverse-Integer.py
+++ Python/000/007-Reverse-Integer.py
@@ -51,6 +51,28 @@ class Solution:
 
         return res
 
+    def reverse_v4(self, x: int) -> int:
+        """
+        This method is similar to reverse_v3 but uses string manipulation.
+        """
+        MAX = 2 ** 31 - 1
+        MIN = -2 ** 31
+
+        res = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        while x != 0:
+            digit = x % 10
+            x = x // 10
+
+            if res > MAX // 10 or (res == MAX // 10 and digit > MAX % 10):
+                return 0
+
+            res = res * 10 + digit
+
+        return sign * res
+
 
 class TestSolution(unittest.TestCase):
     def setUp(self):
@@ -77,4 +99,9 @@ class TestSolution(unittest.TestCase):
     def test_reverse_v3(self):
         for x, expected in self.test_cases:
             result = self.solution.reverse_v3(x)
+            self.assertEqual(result, expected)
+
+    def test_reverse_v4(self):
+        for x, expected in self.test_cases:
+            result = self.solution.reverse_v4(x)
             self.assertEqual(result, expected)
