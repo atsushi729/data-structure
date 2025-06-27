22f385005eddb23771dacdc4f231b2059c4d05e7
Atsushi <atu729@icloud.com>
2025-06-27 23:51:48 +0900

    add findItinerary2 method and unit tests for alternative implementation of Reconstruct Itinerary problem

--- Python/300/332-Reconstruct-Itinerary.py
+++ Python/300/332-Reconstruct-Itinerary.py
@@ -1,4 +1,5 @@
 from typing import List
+from collections import defaultdict
 import unittest
 
 
@@ -28,6 +29,22 @@ class Solution:
         dfs("JFK")
         return res
 
+    def findItinerary2(self, tickets: List[List[str]]) -> List[str]:
+        adj = defaultdict(list)
+        for src, dst in sorted(tickets)[::-1]:
+            adj[src].append(dst)
+
+        stack = ["JFK"]
+        res = []
+
+        while stack:
+            cur = stack[-1]
+            if not adj[cur]:
+                res.append(stack.pop())
+            else:
+                stack.append(adj[cur].pop())
+        return res[::-1]
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -46,4 +63,10 @@ class TestSolution(unittest.TestCase):
         for tickets, expected in self.test_cases:
             with self.subTest(tickets=tickets, expected=expected):
                 result = self.solution.findItinerary(tickets)
+                self.assertEqual(result, expected)
+
+    def test_findItinerary2(self):
+        for tickets, expected in self.test_cases:
+            with self.subTest(tickets=tickets, expected=expected):
+                result = self.solution.findItinerary2(tickets)
                 self.assertEqual(result, expected)
