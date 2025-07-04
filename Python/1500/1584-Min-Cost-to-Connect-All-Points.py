9054315f86186b93e7471ca1f762851426fb254e
Atsushi <atu729@icloud.com>
2025-07-04 22:28:50 +0900

    add minCostConnectPoints3 method and unit tests for alternative implementation of Min Cost to Connect All Points problem

--- Python/1500/1584-Min-Cost-to-Connect-All-Points.py
+++ Python/1500/1584-Min-Cost-to-Connect-All-Points.py
@@ -53,6 +53,30 @@ class Solution:
 
         return res
 
+    def minCostConnectPoints3(self, points: List[List[int]]) -> int:
+        n, node = len(points), 0
+        dist = [100000000] * n
+        visit = [False] * n
+        edges, res = 0, 0
+
+        while edges < n - 1:
+            visit[node] = True
+            nextNode = -1
+            for i in range(n):
+                if visit[i]:
+                    continue
+                curDist = (abs(points[i][0] - points[node][0]) +
+                           abs(points[i][1] - points[node][1]))
+                dist[i] = min(dist[i], curDist)
+                if nextNode == -1 or dist[i] < dist[nextNode]:
+                    nextNode = i
+
+            res += dist[nextNode]
+            node = nextNode
+            edges += 1
+
+        return res
+
 
 class DSU:
     def __init__(self, n):
@@ -118,6 +142,12 @@ class TestSolution(unittest.TestCase):
         for points, expected in self.test_cases:
             with self.subTest(points=points, expected=expected):
                 result = self.solution.minCostConnectPoints2(points)
+                self.assertEqual(result, expected)
+
+    def test_minCostConnectPoints3(self):
+        for points, expected in self.test_cases:
+            with self.subTest(points=points, expected=expected):
+                result = self.solution.minCostConnectPoints3(points)
                 self.assertEqual(result, expected)
 
     def test_minCostConnectPoints_dsu(self):
