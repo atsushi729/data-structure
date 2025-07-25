a4bd508fe109b3ee3321210abe58346b438b3c36
Atsushi <atu729@icloud.com>
2025-07-25 23:19:16 +0900

    add longest_increasing_path2 method and corresponding unit tests for Longest Increasing Path in a Matrix problem

--- Python/300/329-Longest-Increasing-Path-in-a-Matrix.py
+++ Python/300/329-Longest-Increasing-Path-in-a-Matrix.py
@@ -28,6 +28,27 @@ class Solution:
 
         return max(dp.values())
 
+    def longest_increasing_path2(self, matrix: List[List[int]]) -> int:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+        def dfs(r, c, prevVal):
+            if (min(r, c) < 0 or r >= ROWS or
+                    c >= COLS or matrix[r][c] <= prevVal
+            ):
+                return 0
+
+            res = 1
+            for d in directions:
+                res = max(res, 1 + dfs(r + d[0], c + d[1], matrix[r][c]))
+            return res
+
+        LIP = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                LIP = max(LIP, dfs(r, c, float('-inf')))
+        return LIP
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -44,4 +65,10 @@ class TestSolution(unittest.TestCase):
         for matrix, expected in self.test_cases:
             with self.subTest(matrix=matrix):
                 result = self.solution.longest_increasing_path(matrix)
+                self.assertEqual(result, expected)
+
+    def test_longest_increasing_path2(self):
+        for matrix, expected in self.test_cases:
+            with self.subTest(matrix=matrix):
+                result = self.solution.longest_increasing_path2(matrix)
                 self.assertEqual(result, expected)
