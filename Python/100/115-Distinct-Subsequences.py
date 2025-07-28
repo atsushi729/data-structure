f3cb1e57381025054a9472a2a9d159f90e8740db
Atsushi <atu729@icloud.com>
2025-07-28 23:46:55 +0900

    add numDistinct method and corresponding unit tests for Distinct Subsequences problem

---
+++ Python/100/115-Distinct-Subsequences.py
@@ -0,0 +1,46 @@
+import unittest
+
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        if len(t) > len(s):
+            return 0
+
+        dp = {}
+
+        def dfs(i, j):
+            if j == len(t):
+                return 1
+            if i == len(s):
+                return 0
+            if (i, j) in dp:
+                return dp[(i, j)]
+
+            res = dfs(i + 1, j)
+            if s[i] == t[j]:
+                res += dfs(i + 1, j + 1)
+            dp[(i, j)] = res
+            return res
+
+        return dfs(0, 0)
+
+
+class TestSolution(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.solution = Solution()
+        cls.test_cases = [
+            ("rabbbit", "rabbit", 3),
+            ("babgbag", "bag", 5),
+            ("a", "a", 1),
+            ("a", "b", 0),
+            ("", "", 1),
+            ("abc", "", 1),
+            ("", "abc", 0)
+        ]
+
+    def test_num_distinct(self):
+        for s, t, expected in self.test_cases:
+            with self.subTest(s=s, t=t):
+                result = self.solution.numDistinct(s, t)
+                self.assertEqual(result, expected, f"Failed for s: {s}, t: {t}. Expected {expected}, got {result}.")
