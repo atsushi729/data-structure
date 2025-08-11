452ea2a78639c7bc316070d7500e316734dde7db
Atsushi <atu729@icloud.com>
2025-08-11 23:42:43 +0900

    add max_coins3 method and corresponding unit tests for Burst Balloons problem

--- Python/300/312-Burst-Balloons.py
+++ Python/300/312-Burst-Balloons.py
@@ -37,6 +37,20 @@ class Solution:
 
         return dfs(nums)
 
+    def max_coins3(self, nums: List[int]) -> int:
+        n = len(nums)
+        new_nums = [1] + nums + [1]
+
+        dp = [[0] * (n + 2) for _ in range(n + 2)]
+        for l in range(n, 0, -1):
+            for r in range(l, n + 1):
+                for i in range(l, r + 1):
+                    coins = new_nums[l - 1] * new_nums[i] * new_nums[r + 1]
+                    coins += dp[l][i - 1] + dp[i + 1][r]
+                    dp[l][r] = max(dp[l][r], coins)
+
+        return dp[1][n]
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -58,3 +72,8 @@ class TestSolution(unittest.TestCase):
         for nums, expected in self.test_cases:
             with self.subTest(nums=nums):
                 self.assertEqual(self.solution.max_coins2(nums), expected)
+
+    def test_max_coins3(self):
+        for nums, expected in self.test_cases:
+            with self.subTest(nums=nums):
+                self.assertEqual(self.solution.max_coins3(nums), expected)
