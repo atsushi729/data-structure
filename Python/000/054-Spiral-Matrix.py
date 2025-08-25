e5f1c441b416edf47eb523e3c60230cdac821e6a
Atsushi <atu729@icloud.com>
2025-08-25 23:31:29 +0900

    add spiralOrder3 method and unit tests for alternative matrix traversal approach

--- Python/000/054-Spiral-Matrix.py
+++ Python/000/054-Spiral-Matrix.py
@@ -47,6 +47,21 @@ class Solution:
         dfs(m, n, 0, -1, 0, 1)
         return res
 
+    def spiralOrder3(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        steps = [len(matrix[0]), len(matrix) - 1]
+
+        r, c, d = 0, -1, 0
+        while steps[d & 1]:
+            for i in range(steps[d & 1]):
+                r += directions[d][0]
+                c += directions[d][1]
+                res.append(matrix[r][c])
+            steps[d & 1] -= 1
+            d += 1
+            d %= 4
+        return res
 
 class TestSolution(unittest.TestCase):
 
@@ -90,4 +105,10 @@ class TestSolution(unittest.TestCase):
         for matrix, expected in self.test_cases:
             with self.subTest(matrix=matrix):
                 result = self.sol.spiralOrder2(matrix)
+                self.assertEqual(result, expected)
+
+    def test_spiralOrder3(self):
+        for matrix, expected in self.test_cases:
+            with self.subTest(matrix=matrix):
+                result = self.sol.spiralOrder3(matrix)
                 self.assertEqual(result, expected)
