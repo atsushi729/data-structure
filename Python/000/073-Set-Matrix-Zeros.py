b8f861c528570e13929e58321c2b9b902d468dd5
Atsushi <atu729@icloud.com>
2025-08-26 23:36:54 +0900

    add set_zeroes2 method and unit tests for alternative matrix zeroing approach

--- Python/000/073-Set-Matrix-Zeros.py
+++ Python/000/073-Set-Matrix-Zeros.py
@@ -30,6 +30,22 @@ def set_zeroes(matrix: list[list[int]]) -> None:
             matrix[row][col_index] = 0
 
 
+def set_zeroes2(matrix: list[list[int]]) -> None:
+    ROWS, COLS = len(matrix), len(matrix[0])
+    rows, cols = [False] * ROWS, [False] * COLS
+
+    for r in range(ROWS):
+        for c in range(COLS):
+            if matrix[r][c] == 0:
+                rows[r] = True
+                cols[c] = True
+
+    for r in range(ROWS):
+        for c in range(COLS):
+            if rows[r] or cols[c]:
+                matrix[r][c] = 0
+
+
 #################### Test Case ####################
 class TestSetZeroes(unittest.TestCase):
     def test_set_zeroes(self):
@@ -39,4 +55,13 @@ class TestSetZeroes(unittest.TestCase):
 
         matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
         set_zeroes(matrix)
+        self.assertEqual(matrix, [[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]])
+
+    def test_set_zeroes2(self):
+        matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+        set_zeroes2(matrix)
+        self.assertEqual(matrix, [[1, 0, 1], [0, 0, 0], [1, 0, 1]])
+
+        matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]]
+        set_zeroes2(matrix)
         self.assertEqual(matrix, [[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]])
