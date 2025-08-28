766e2f68336d9dd8682f7069aa8b6519eeaa0f5b
Atsushi <atu729@icloud.com>
2025-08-28 21:11:50 +0900

    add subsets_v4 method and unit tests for generating all subsets using backtracking

--- Python/000/078-Subsets.py
+++ Python/000/078-Subsets.py
@@ -39,6 +39,23 @@ class Solution:
             res.append(subset)
         return res
 
+    def subsets_v4(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res: List[List[int]] = []
+        cur: List[int] = []
+
+        def backtrack(i: int) -> None:
+            if i == n:
+                res.append(cur[:])
+                return
+            cur.append(nums[i])
+            backtrack(i + 1)
+            cur.pop()
+            backtrack(i + 1)
+
+        backtrack(0)
+        return res
+
 
 ##################### Test Case ####################
 class TestSolution(unittest.TestCase):
@@ -59,3 +76,9 @@ class TestSolution(unittest.TestCase):
         self.assertListEqual(solution.subset_v3([1, 2, 3]), [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]])
         self.assertListEqual(solution.subset_v3([0]), [[], [0]])
         self.assertListEqual(solution.subset_v3([]), [[]])
+
+    def test_subset_v4(self):
+        solution = Solution()
+        self.assertListEqual(solution.subsets_v4([1, 2, 3]), [[1, 2, 3], [1, 2], [1, 3], [1], [2, 3], [2], [3], []])
+        self.assertListEqual(solution.subsets_v4([0]), [[0], []])
+        self.assertListEqual(solution.subsets_v4([]), [[]])
