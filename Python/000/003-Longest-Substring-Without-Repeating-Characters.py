08030abb88d6d11d5b8e7d02f3dfdbdad218bee2
Atsushi <atu729@icloud.com>
2025-09-01 23:48:46 +0900

    refactor length_of_longest_substring methods to be part of Solution class and update unit tests

--- Python/000/003-Longest-Substring-Without-Repeating-Characters.py
+++ Python/000/003-Longest-Substring-Without-Repeating-Characters.py
@@ -2,59 +2,64 @@ import unittest
 
 
 #################### Solution ####################
-def length_of_longest_substring(s: str) -> int:
-    current_max = 0
-    box = []
+class Solution:
+    def length_of_longest_substring(self, s: str) -> int:
+        current_max = 0
+        box = []
 
-    for char in s:
-        box.append(char)
+        for char in s:
+            box.append(char)
 
-        if len(list(set(box))) == len(box):
-            current_max = max(current_max, len(box))
-        else:
-            index = box.index(char) + 1
-            box = box[index:]
+            if len(list(set(box))) == len(box):
+                current_max = max(current_max, len(box))
+            else:
+                index = box.index(char) + 1
+                box = box[index:]
 
-    current_max = max(current_max, len(box))
-    return current_max
+        current_max = max(current_max, len(box))
+        return current_max
 
+    def length_of_longest_substring_v2(self, s: str) -> int:
+        if not s:
+            return 0
 
-def length_of_longest_substring_v2(s: str) -> int:
-    if not s:
-        return 0
+        seen = {}
+        start = 0
+        longest = 0
 
-    seen = {}
-    start = 0
-    longest = 0
+        for i, c in enumerate(s):
+            if c in seen and seen[c] >= start:
+                start = seen[c] + 1
+            else:
+                longest = max(longest, i - start + 1)
+            seen[c] = i
 
-    for i, c in enumerate(s):
-        if c in seen and seen[c] >= start:
-            start = seen[c] + 1
-        else:
-            longest = max(longest, i - start + 1)
-        seen[c] = i
-
-    return longest
+        return longest
 
 
 #################### Test Case ####################
 class TestLengthOfLongestSubstring(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.solution = Solution()
+        cls.test_cases = [
+            ("abcabcbb", 3),
+            ("bbbbb", 1),
+            ("pwwkew", 3),
+            ("", 0),
+            ("au", 2),
+            ("dvdf", 3),
+            ("anviaj", 5),
+        ]
+
     def test_length_of_longest_substring(self):
-        self.assertEqual(length_of_longest_substring("abcabcbb"), 3)
-        self.assertEqual(length_of_longest_substring("bbbbb"), 1)
-        self.assertEqual(length_of_longest_substring("pwwkew"), 3)
-        self.assertEqual(length_of_longest_substring(""), 0)
-        self.assertEqual(length_of_longest_substring(" "), 1)
-        self.assertEqual(length_of_longest_substring("au"), 2)
-        self.assertEqual(length_of_longest_substring("dvdf"), 3)
-        self.assertEqual(length_of_longest_substring("abba"), 2)
+        for s, expected in self.test_cases:
+            with self.subTest(s=s):
+                result = self.solution.length_of_longest_substring(s)
+                self.assertEqual(result, expected)
 
     def test_length_of_longest_substring_v2(self):
-        self.assertEqual(length_of_longest_substring_v2("abcabcbb"), 3)
-        self.assertEqual(length_of_longest_substring_v2("bbbbb"), 1)
-        self.assertEqual(length_of_longest_substring_v2("pwwkew"), 3)
-        self.assertEqual(length_of_longest_substring_v2(""), 0)
-        self.assertEqual(length_of_longest_substring_v2(" "), 1)
-        self.assertEqual(length_of_longest_substring_v2("au"), 2)
-        self.assertEqual(length_of_longest_substring_v2("dvdf"), 3)
-        self.assertEqual(length_of_longest_substring_v2("abba"), 2)
+        for s, expected in self.test_cases:
+            with self.subTest(s=s):
+                result = self.solution.length_of_longest_substring_v2(s)
+                self.assertEqual(result, expected)
