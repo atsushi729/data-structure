96e8f94df2e0faa520bf825944722abb6d7cc64c
Atsushi <atu729@icloud.com>
2025-09-24 20:18:35 +0900

    add merge_v3 method for merging intervals with optimized logic and unit tests

--- Python/000/056-Merge-Intervals.py
+++ Python/000/056-Merge-Intervals.py
@@ -38,6 +38,31 @@ class Solution:
                 intervals = []
         return res
 
+    def merge_v3(self, intervals: List[List[int]]) -> List[List[int]]:
+        max_val = max(interval[0] for interval in intervals)
+
+        mp = [0] * (max_val + 1)
+        for start, end in intervals:
+            mp[start] = max(end + 1, mp[start])
+
+        res = []
+        have = -1
+        interval_start = -1
+        for i in range(len(mp)):
+            if mp[i] != 0:
+                if interval_start == -1:
+                    interval_start = i
+                have = max(mp[i] - 1, have)
+            if have == i:
+                res.append([interval_start, have])
+                have = -1
+                interval_start = -1
+
+        if interval_start != -1:
+            res.append([interval_start, have])
+
+        return res
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -72,4 +97,10 @@ class TestSolution(unittest.TestCase):
         for intervals, expected in self.test_cases:
             with self.subTest(intervals=intervals, expected=expected):
                 result = self.solution.merge_v2(intervals)
+                self.assertEqual(result, expected)
+
+    def test_merge_v3(self):
+        for intervals, expected in self.test_cases:
+            with self.subTest(intervals=intervals, expected=expected):
+                result = self.solution.merge_v3(intervals)
                 self.assertEqual(result, expected)
