2202bca050494487a7c9b567e68167b6bac9aa17
Atsushi <atu729@icloud.com>
2025-09-25 22:47:54 +0900

    add solution for meeting rooms problem with unit tests

---
+++ Python/200/252-Meeting-Rooms.py
@@ -0,0 +1,50 @@
+from typing import List
+import unittest
+
+
+# Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+# Solution:
+class Solution:
+    def can_attend_meetings(self, intervals: List[Interval]) -> bool:
+        if not intervals:
+            return True
+
+        intervals.sort(key=lambda x: x.start)
+        prev_end = intervals[0].end
+
+        for i in range(1, len(intervals)):
+            if intervals[i].start < prev_end:
+                return False
+            prev_end = intervals[i].end
+        return True
+
+
+class TestSolution(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.solution = Solution()
+        cls.test_cases = [
+            (
+                [Interval(0, 30), Interval(5, 10), Interval(15, 20)],
+                False,
+            ),
+            (
+                [Interval(7, 10), Interval(2, 4)],
+                True,
+            ),
+            (
+                [],
+                True,
+            ),
+        ]
+
+    def test_can_attend_meetings(self):
+        for intervals, expected in self.test_cases:
+            with self.subTest(intervals=intervals):
+                result = self.solution.can_attend_meetings(intervals)
+                self.assertEqual(result, expected)
