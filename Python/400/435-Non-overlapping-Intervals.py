50afd755677b0521a5f05845b7e06e6a28902c4a
Atsushi <atu729@icloud.com>
2025-10-01 20:51:06 +0900

    add erase_overlap_intervals_v4 method for optimized overlap removal with unit tests

--- Python/400/435-Non-overlapping-Intervals.py
+++ Python/400/435-Non-overlapping-Intervals.py
@@ -41,6 +41,23 @@ class Solution:
 
         return len(intervals) - dfs(0, -1)
 
+    def erase_overlap_intervals_v4(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
+        n = len(intervals)
+        memo = {}
+
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            res = 1
+            for j in range(i + 1, n):
+                if intervals[i][1] <= intervals[j][0]:
+                    res = max(res, 1 + dfs(j))
+            memo[i] = res
+            return res
+
+        return n - dfs(0)
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -90,5 +107,13 @@ class TestSolution(unittest.TestCase):
             with self.subTest(intervals=intervals):
                 self.assertEqual(
                     self.solution.erase_overlap_intervals_v3(intervals),
+                    expected,
+                )
+
+    def test_erase_overlap_intervals_v4(self):
+        for intervals, expected in self.test_cases:
+            with self.subTest(intervals=intervals):
+                self.assertEqual(
+                    self.solution.erase_overlap_intervals_v4(intervals),
                     expected,
                 )
