1c346551d7c76d1c4d84c89f661c00ea7f1555e4
Atsushi <atu729@icloud.com>
2025-10-11 08:52:04 +0900

    add HashMapV2 class with basic operations and unit tests

--- Python/700/706-Design-HashMap.py
+++ Python/700/706-Design-HashMap.py
@@ -18,6 +18,20 @@ class MyHashMap:
             self.hash_list.pop(key)
 
 
+class HashMapV2:
+    def __init__(self):
+        self.map = [-1] * 1000001
+
+    def put(self, key: int, value: int) -> None:
+        self.map[key] = value
+
+    def get(self, key: int) -> int:
+        return self.map[key]
+
+    def remove(self, key: int) -> None:
+        self.map[key] = -1
+
+
 class TestMyHashMap(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -48,4 +62,17 @@ class TestMyHashMap(unittest.TestCase):
                     results.append(self.hash_map.get(*values))
                 elif op == "remove":
                     results.append(self.hash_map.remove(*values))
+            self.assertEqual(results, expected)
+
+    def test_hash_map_v2(self):
+        hash_map_v2 = HashMapV2()
+        for operations, expected in self.test_cases:
+            results = []
+            for op, *values in operations:
+                if op == "put":
+                    results.append(hash_map_v2.put(*values))
+                elif op == "get":
+                    results.append(hash_map_v2.get(*values))
+                elif op == "remove":
+                    results.append(hash_map_v2.remove(*values))
             self.assertEqual(results, expected)
