4417384d2274ff4a2cc0a718e047ea4cd845b6d9
Atsushi <atu729@icloud.com>
2025-10-13 11:57:45 +0900

    add HashMapV3 class with basic operations and unit tests

--- Python/700/706-Design-HashMap.py
+++ Python/700/706-Design-HashMap.py
@@ -32,6 +32,48 @@ class HashMapV2:
         self.map[key] = -1
 
 
+class ListNode:
+    def __init__(self, key=-1, val=-1):
+        self.key = key
+        self.val = val
+        self.next = None
+
+
+class HashMapV3:
+    class MyHashMap:
+
+        def __init__(self):
+            self.map = [ListNode() for _ in range(1000)]
+
+        def hash(self, key: int) -> int:
+            return key % len(self.map)
+
+        def put(self, key: int, value: int) -> None:
+            cur = self.map[self.hash(key)]
+            while cur.next:
+                if cur.next.key == key:
+                    cur.next.val = value
+                    return
+                cur = cur.next
+            cur.next = ListNode(key, value)
+
+        def get(self, key: int) -> int:
+            cur = self.map[self.hash(key)].next
+            while cur:
+                if cur.key == key:
+                    return cur.val
+                cur = cur.next
+            return -1
+
+        def remove(self, key: int) -> None:
+            cur = self.map[self.hash(key)]
+            while cur.next:
+                if cur.next.key == key:
+                    cur.next = cur.next.next
+                    return
+                cur = cur.next
+
+
 class TestMyHashMap(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -75,4 +117,17 @@ class TestMyHashMap(unittest.TestCase):
                     results.append(hash_map_v2.get(*values))
                 elif op == "remove":
                     results.append(hash_map_v2.remove(*values))
+            self.assertEqual(results, expected)
+
+    def test_hash_map_v3(self):
+        hash_map_v3 = HashMapV3.MyHashMap()
+        for operations, expected in self.test_cases:
+            results = []
+            for op, *values in operations:
+                if op == "put":
+                    results.append(hash_map_v3.put(*values))
+                elif op == "get":
+                    results.append(hash_map_v3.get(*values))
+                elif op == "remove":
+                    results.append(hash_map_v3.remove(*values))
             self.assertEqual(results, expected)
