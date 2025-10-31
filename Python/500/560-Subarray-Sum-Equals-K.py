2f1065d19e1aa0e7f3169161a97e675bc3d23513
Atsushi <atu729@icloud.com>
2025-10-31 22:58:14 +0900

    add subarray_sum_v2 method for counting subarrays that sum to k and corresponding unit tests

--- Python/500/560-Subarray-Sum-Equals-K.py
+++ Python/500/560-Subarray-Sum-Equals-K.py
@@ -16,6 +16,20 @@ class Solution:
 
         return res
 
+    def subarray_sum_v2(self, nums: List[int], k: int) -> int:
+        prefix_sum_count = {0: 1}
+        prefix_sum = 0
+        res = 0
+
+        for num in nums:
+            prefix_sum += num
+            complement = prefix_sum - k
+
+            res += prefix_sum_count.get(complement, 0)
+            prefix_sum_count[prefix_sum] = prefix_sum_count.get(prefix_sum, 0) + 1
+
+        return res
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -33,3 +47,8 @@ class TestSolution(unittest.TestCase):
         for nums, k, expected in self.test_cases:
             with self.subTest(nums=nums, k=k, expected=expected):
                 self.assertEqual(self.s.subarray_sum(nums, k), expected)
+
+    def test_subarraySum_v2(self):
+        for nums, k, expected in self.test_cases:
+            with self.subTest(nums=nums, k=k, expected=expected):
+                self.assertEqual(self.s.subarray_sum_v2(nums, k), expected)
