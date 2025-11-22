60ce447c0b5294130e00d7bc40513c1df5fff864
Atsushi <atu729@icloud.com>
2025-11-23 08:49:23 +0900

    add four_sum_v3 method implementing k-sum algorithm and corresponding unit tests

--- Python/000/018-4Sum.py
+++ Python/000/018-4Sum.py
@@ -44,6 +44,42 @@ class Solution:
                         right -= 1
         return list(res)
 
+    def four_sum_v3(self, nums: List[int], target: int) -> List[List[int]]:
+        """
+        Time complexity: O(n^(k-1))
+        Space complexity: O(n)
+        """
+        nums.sort()
+        res, quad = [], []
+
+        def k_sum(k, start, target):
+            if k == 2:
+                l, r = start, len(nums) - 1
+                while l < r:
+                    if nums[l] + nums[r] < target:
+                        l += 1
+                    elif nums[l] + nums[r] > target:
+                        r -= 1
+                    else:
+                        res.append(quad + [nums[l], nums[r]])
+                        l += 1
+                        r -= 1
+                        while l < r and nums[l] == nums[l - 1]:
+                            l += 1
+                        while l < r and nums[r] == nums[r + 1]:
+                            r -= 1
+                return
+
+            for i in range(start, len(nums) - k + 1):
+                if i > start and nums[i] == nums[i - 1]:
+                    continue
+                quad.append(nums[i])
+                k_sum(k - 1, i + 1, target - nums[i])
+                quad.pop()
+
+        k_sum(4, 0, target)
+        return res
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -52,6 +88,10 @@ class TestSolution(unittest.TestCase):
         cls.test_cases = [
             ([1, 0, -1, 0, -2, 2], 0, [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]),
             ([2, 2, 2, 2, 2], 8, [[2, 2, 2, 2]]),
+            ([], 0, []),
+            ([0, 0, 0, 0], 0, [[0, 0, 0, 0]]),
+            ([1, 2, 3, 4], 10, [[1, 2, 3, 4]]),
+            ([1, 2, 3, 4], 100, []),
         ]
 
     def test_four_sum(self):
@@ -66,6 +106,14 @@ class TestSolution(unittest.TestCase):
         for nums, target, expected in self.test_cases:
             with self.subTest(nums=nums, target=target):
                 result = self.s.four_sum_v2(nums, target)
+                result_sorted = [sorted(quad) for quad in result]
+                expected_sorted = [sorted(quad) for quad in expected]
+                self.assertCountEqual(result_sorted, expected_sorted)
+
+    def test_four_sum_v3(self):
+        for nums, target, expected in self.test_cases:
+            with self.subTest(nums=nums, target=target):
+                result = self.s.four_sum_v3(nums, target)
                 result_sorted = [sorted(quad) for quad in result]
                 expected_sorted = [sorted(quad) for quad in expected]
                 self.assertCountEqual(result_sorted, expected_sorted)
