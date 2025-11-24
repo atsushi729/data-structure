9add50825e2bfae9fce7f628a5c1c2e2b719ff5a
Atsushi <atu729@icloud.com>
2025-11-24 12:26:54 +0900

    add four_sum_v4 method implementing k-sum algorithm with unit tests

--- Python/000/018-4Sum.py
+++ Python/000/018-4Sum.py
@@ -80,6 +80,41 @@ class Solution:
         k_sum(4, 0, target)
         return res
 
+    def four_sum_v4(self, nums: List[int], target: int) -> List[List[int]]:
+        """
+        Time complexity: O(n^(k-1))
+        Space complexity: O(n)
+        """
+        nums.sort()
+        res, quad = [], []
+
+        def kSum(k, start, target):
+            if k != 2:
+                for i in range(start, len(nums) - k + 1):
+                    if i > start and nums[i] == nums[i - 1]:
+                        continue
+                    quad.append(nums[i])
+                    kSum(k - 1, i + 1, target - nums[i])
+                    quad.pop()
+                return
+
+            # base case, two sum II
+            l, r = start, len(nums) - 1
+            while l < r:
+                if nums[l] + nums[r] < target:
+                    l += 1
+                elif nums[l] + nums[r] > target:
+                    r -= 1
+                else:
+                    res.append(quad + [nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+
+        kSum(4, 0, target)
+        return res
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -114,6 +149,14 @@ class TestSolution(unittest.TestCase):
         for nums, target, expected in self.test_cases:
             with self.subTest(nums=nums, target=target):
                 result = self.s.four_sum_v3(nums, target)
+                result_sorted = [sorted(quad) for quad in result]
+                expected_sorted = [sorted(quad) for quad in expected]
+                self.assertCountEqual(result_sorted, expected_sorted)
+
+    def test_four_sum_v4(self):
+        for nums, target, expected in self.test_cases:
+            with self.subTest(nums=nums, target=target):
+                result = self.s.four_sum_v4(nums, target)
                 result_sorted = [sorted(quad) for quad in result]
                 expected_sorted = [sorted(quad) for quad in expected]
                 self.assertCountEqual(result_sorted, expected_sorted)
