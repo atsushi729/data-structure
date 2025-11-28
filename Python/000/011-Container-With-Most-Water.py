3fb959dc3b122a4e0818f965353aa8c76156e435
Atsushi <atu729@icloud.com>
2025-11-28 22:13:09 +0900

    refactor max_area and model_max_area methods to be instance methods in Solution class; update tests accordingly

--- Python/000/011-Container-With-Most-Water.py
+++ Python/000/011-Container-With-Most-Water.py
@@ -2,43 +2,59 @@ import unittest
 
 
 #################### Solution ####################
-def max_area(heights: list[int]) -> int:
-    max_amount = 0
-    left, right = 0, len(heights) - 1
+class Solution:
+    def max_area(self, heights: list[int]) -> int:
+        max_amount = 0
+        left, right = 0, len(heights) - 1
 
-    while left < right:
-        width = right - left
-        min_height = min(heights[left], heights[right])
-        current_amount = width * min_height
+        while left < right:
+            width = right - left
+            min_height = min(heights[left], heights[right])
+            current_amount = width * min_height
 
-        max_amount = max(max_amount, current_amount)
+            max_amount = max(max_amount, current_amount)
 
-        if heights[left] < heights[right]:
-            left += 1
-        else:
-            right -= 1
+            if heights[left] < heights[right]:
+                left += 1
+            else:
+                right -= 1
 
-    return max_amount
+        return max_amount
 
+    def model_max_area(self, heights: list[int]) -> int:
+        l, r = 0, len(heights) - 1
+        res = 0
 
-def model_max_area(heights: list[int]) -> int:
-    l, r = 0, len(heights) - 1
-    res = 0
-
-    while l < r:
-        area = min(heights[l], heights[r]) * (r - l)
-        res = max(res, area)
-        if heights[l] <= heights[r]:
-            l += 1
-        else:
-            r -= 1
-    return res
+        while l < r:
+            area = min(heights[l], heights[r]) * (r - l)
+            res = max(res, area)
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
 
 
 #################### Test Case ####################
 class TestMaxArea(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.s = Solution()
+        cls.test_cases = [
+            ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
+            ([1, 1], 1),
+            ([4, 3, 2, 1, 4], 16),
+            ([1, 2, 1], 2),
+        ]
+
     def test_max_area(self):
-        self.assertEqual(max_area([1, 8, 6, 2, 5, 4, 8, 3, 7]), 49)
+        for heights, expected in self.test_cases:
+            with self.subTest(heights=heights):
+                result = self.s.max_area(heights)
+                self.assertEqual(result, expected)
 
     def test_model_max_area(self):
-        self.assertEqual(model_max_area([1, 8, 6, 2, 5, 4, 8, 3, 7]), 49)
+        for heights, expected in self.test_cases:
+            with self.subTest(heights=heights):
+                result = self.s.model_max_area(heights)
+                self.assertEqual(result, expected)
