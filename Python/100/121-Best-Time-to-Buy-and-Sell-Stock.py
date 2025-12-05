5d3ce1114f9594a558d0f174c563b83e1301d1dd
Atsushi <atu729@icloud.com>
2025-12-05 22:34:50 +0900

    add max_profit method for calculating maximum stock profit with unit tests

--- Python/100/121-Best-Time-to-Buy-and-Sell-Stock.py
+++ Python/100/121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,16 +1,34 @@
-def maxProfit(prices: list[int]) -> int:
-    # Store profit as list
-    profit = []
+import unittest
+from typing import List
 
-    for i in range(len(prices)):
-        current_day_price = prices[i]
-        max_price_day_after_current_day = max(prices[i:])
 
-        current_profit = max_price_day_after_current_day - current_day_price
-        profit.append(current_profit)
+class Solution:
+    def max_profit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        max_profit = 0
 
-    max_profit = max(profit)
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                max_profit = max(max_profit, profit)
+            else:
+                l = r
+            r += 1
+        return max_profit
 
-    if max_profit <= 0:
-        return 0
-    return max_profit
+
+class TestSolution(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.s = Solution()
+        cls.test_cases = [
+            ([7, 1, 5, 3, 6, 4], 5),
+            ([7, 6, 4, 3, 1], 0),
+            ([1, 2, 3, 4, 5], 4),
+            ([3, 2, 6, 5, 0, 3], 4),
+        ]
+
+    def test_max_profit(self):
+        for prices, expected in self.test_cases:
+            with self.subTest(prices=prices, expected=expected):
+                self.assertEqual(self.s.max_profit(prices), expected)
