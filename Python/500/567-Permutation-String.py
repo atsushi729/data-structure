6958407078a0e52f12b0b31c3a15db2984355db4
Atsushi <atu729@icloud.com>
2025-12-12 22:54:18 +0900

    refactor check_inclusion and model_check_inclusion methods into Solution class with unit tests

--- Python/500/567-Permutation-String.py
+++ Python/500/567-Permutation-String.py
@@ -2,57 +2,67 @@ import unittest
 
 
 #################### Solution ####################
-def check_inclusion(s1: str, s2: str) -> bool:
-    if len(s2) < len(s1):
+class Solution:
+    def check_inclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+
+        sorted_s1 = sorted(s1)
+
+        for i in range(len(s2) - len(s1) + 1):
+            target_char = s2[i:len(s1) + i]
+            sorted_target = sorted(target_char)
+
+            if sorted_target == sorted_s1:
+                return True
+
         return False
 
-    sorted_s1 = sorted(s1)
+    def model_check_inclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
 
-    for i in range(len(s2) - len(s1) + 1):
-        target_char = s2[i:len(s1) + i]
-        sorted_target = sorted(target_char)
+        s1_map = {}
+        s2_map = {}
+        for i in range(26):
+            s1_map[chr(97 + i)] = 0
+            s2_map[chr(97 + i)] = 0
 
-        if sorted_target == sorted_s1:
-            return True
-
-    return False
-
-
-def model_check_inclusion(s1: str, s2: str) -> bool:
-    if len(s2) < len(s1):
-        return False
-
-    s1_map = {}
-    s2_map = {}
-    for i in range(26):
-        s1_map[chr(97 + i)] = 0
-        s2_map[chr(97 + i)] = 0
-
-    for i in range(len(s1)):
-        s1_map[s1[i]] += 1
-        s2_map[s2[i]] += 1
-
-    if s1_map == s2_map:
-        return True
-
-    for i in range(len(s1), len(s2)):
-        s2_map[s2[i]] += 1
-        s2_map[s2[i - len(s1)]] -= 1
+        for i in range(len(s1)):
+            s1_map[s1[i]] += 1
+            s2_map[s2[i]] += 1
 
         if s1_map == s2_map:
             return True
 
-    return False
+        for i in range(len(s1), len(s2)):
+            s2_map[s2[i]] += 1
+            s2_map[s2[i - len(s1)]] -= 1
+
+            if s1_map == s2_map:
+                return True
+
+        return False
 
 
 #################### Test Case ####################
 class TestCheckInclusion(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.solution = Solution()
+        cls.test_cases = [
+            ("ab", "eidbaooo", True),
+            ("ab", "eidboaoo", False),
+            ("adc", "dcda", True),
+            ("hello", "ooolleoooleh", False),
+            ("a", "a", True),
+            ("a", "b", False),
+        ]
+
     def test_check_inclusion(self):
-        self.assertEqual(check_inclusion("ab", "eidbaooo"), True)
-        self.assertEqual(check_inclusion("ab", "eidboaoo"), False)
-        self.assertEqual(check_inclusion("adc", "dcda"), True)
+        for s1, s2, expected in self.test_cases:
+            self.assertEqual(self.solution.check_inclusion(s1, s2), expected)
 
     def test_model_check_inclusion(self):
-        self.assertEqual(model_check_inclusion("ab", "eidbaooo"), True)
-        self.assertEqual(model_check_inclusion("ab", "eidboaoo"), False)
-        self.assertEqual(model_check_inclusion("adc", "dcda"), True)
+        for s1, s2, expected in self.test_cases:
+            self.assertEqual(self.solution.model_check_inclusion(s1, s2), expected)
