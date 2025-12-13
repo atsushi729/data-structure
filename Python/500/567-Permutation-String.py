0b040dd22c86763178e912d9e3a198c0df36066c
Atsushi <atu729@icloud.com>
2025-12-13 11:55:32 +0900

    add check_inclusion_v2 method with unit tests

--- Python/500/567-Permutation-String.py
+++ Python/500/567-Permutation-String.py
@@ -44,6 +44,25 @@ class Solution:
 
         return False
 
+    def check_inclusion_v2(self, s1: str, s2: str) -> bool:
+        count1 = {}
+        for c in s1:
+            count1[c] = 1 + count1.get(c, 0)
+
+        need = len(count1)
+
+        for i in range(len(s2)):
+            count2, cur = {}, 0
+            for j in range(i, len(s2)):
+                count2[s2[j]] = 1 + count2.get(s2[j], 0)
+                if count1.get(s2[j], 0) < count2[s2[j]]:
+                    break
+                if count1.get(s2[j], 0) == count2[s2[j]]:
+                    cur += 1
+                if cur == need:
+                    return True
+        return False
+
 
 #################### Test Case ####################
 class TestCheckInclusion(unittest.TestCase):
@@ -66,3 +85,7 @@ class TestCheckInclusion(unittest.TestCase):
     def test_model_check_inclusion(self):
         for s1, s2, expected in self.test_cases:
             self.assertEqual(self.solution.model_check_inclusion(s1, s2), expected)
+
+    def test_check_inclusion_v2(self):
+        for s1, s2, expected in self.test_cases:
+            self.assertEqual(self.solution.check_inclusion_v2(s1, s2), expected)
