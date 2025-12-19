6edd1d6bfcb20170290d67b8e84b1077d28b5848
Atsushi <atu729@icloud.com>
2025-12-19 12:28:59 +0900

    add find_closest_elements_v2 method with unit tests

--- Python/600/658-Find-K-Closest-Elements.py
+++ Python/600/658-Find-K-Closest-Elements.py
@@ -12,6 +12,10 @@ class Solution:
                 l += 1
         return arr[l:r + 1]
 
+    def find_closest_elements_v2(self, arr: List[int], k: int, x: int) -> List[int]:
+        arr.sort(key=lambda num: (abs(num - x), num))
+        return sorted(arr[:k])
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -29,3 +33,8 @@ class TestSolution(unittest.TestCase):
         for arr, k, x, expected in self.test_cases:
             with self.subTest(arr=arr, k=k, x=x, expected=expected):
                 self.assertEqual(self.s.find_closest_elements(arr, k, x), expected)
+
+    def test_find_closest_elements_v2(self):
+        for arr, k, x, expected in self.test_cases:
+            with self.subTest(arr=arr, k=k, x=x, expected=expected):
+                self.assertEqual(self.s.find_closest_elements_v2(arr, k, x), expected)
