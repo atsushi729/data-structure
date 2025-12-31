d58355de5558d7d14d5d2d24398a03fddd895cb6
Atsushi <atu729@icloud.com>
2025-12-31 22:52:05 +0900

    add MyStackV2 implementation with unit tests in 225-Implement-Stack-using-Queues.py

--- Python/200/225-Implement-Stack-using-Queues.py
+++ Python/200/225-Implement-Stack-using-Queues.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import deque
 
 
 class MyStack:
@@ -18,9 +19,41 @@ class MyStack:
         return len(self.stack) == 0
 
 
+class MyStackV2:
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+
+    def pop(self) -> int:
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+
+
 class TestMyStackCases(unittest.TestCase):
     def test_stack_operations(self):
         obj = MyStack()
+        obj.push(1)
+        obj.push(2)
+        self.assertEqual(obj.top(), 2)
+        self.assertEqual(obj.pop(), 2)
+        self.assertFalse(obj.empty())
+        obj.push(3)
+        self.assertEqual(obj.top(), 3)
+        self.assertEqual(obj.pop(), 3)
+        self.assertEqual(obj.pop(), 1)
+        self.assertTrue(obj.empty())
+
+    def test_stack_v2_operations(self):
+        obj = MyStackV2()
         obj.push(1)
         obj.push(2)
         self.assertEqual(obj.top(), 2)
