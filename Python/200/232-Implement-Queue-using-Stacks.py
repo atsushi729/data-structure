3f012202c526a063ed895c38607e6087c8109e8a
Atsushi <atu729@icloud.com>
2026-01-02 10:28:22 +0900

    add MyQueue2 implementation with unit tests in 232-Implement-Queue-using-Stacks.py

--- Python/200/232-Implement-Queue-using-Stacks.py
+++ Python/200/232-Implement-Queue-using-Stacks.py
@@ -19,9 +19,51 @@ class MyQueue:
         return len(self.queue) == 0
 
 
+class MyQueue2:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        while len(self.stack1) > 1:
+            self.stack2.append(self.stack1.pop())
+        res = self.stack1.pop()
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        return res
+
+    def peek(self) -> int:
+        while len(self.stack1) > 1:
+            self.stack2.append(self.stack1.pop())
+        res = self.stack1[-1]
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        return res
+
+    def empty(self) -> bool:
+        return not self.stack1
+
+
 class TestMyQueueCases(unittest.TestCase):
     def test_queue_operations(self):
         obj = MyQueue()
+        obj.push(1)
+        obj.push(2)
+        self.assertEqual(obj.peek(), 1)
+        self.assertEqual(obj.pop(), 1)
+        self.assertFalse(obj.empty())
+        obj.push(3)
+        self.assertEqual(obj.peek(), 2)
+        self.assertEqual(obj.pop(), 2)
+        self.assertEqual(obj.pop(), 3)
+        self.assertTrue(obj.empty())
+
+    def test_queue_v2_operations(self):
+        obj = MyQueue2()
         obj.push(1)
         obj.push(2)
         self.assertEqual(obj.peek(), 1)
