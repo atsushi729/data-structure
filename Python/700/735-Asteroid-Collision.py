db99818434931c516fde8f80d16a834cad16601c
Atsushi <atu729@icloud.com>
2026-01-09 22:16:15 +0900

    fix asteroid_collision method to improve collision logic

--- Python/700/735-Asteroid-Collision.py
+++ Python/700/735-Asteroid-Collision.py
@@ -6,7 +6,7 @@ class Solution:
     def asteroid_collision(self, asteroids: List[int]) -> List[int]:
         stack = []
         for a in asteroids:
-            while stack and a < 0 and stack[-1] > 0:
+            while stack and a < 0 < stack[-1]:
                 diff = a + stack[-1]
                 if diff < 0:
                     stack.pop()
