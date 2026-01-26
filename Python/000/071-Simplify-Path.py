99eeea103fb9088249576616d978fab1fb88d228
Atsushi <atu729@icloud.com>
2026-01-26 21:14:40 +0900

    add simplify_path_v3 method with corresponding unit tests

--- Python/000/071-Simplify-Path.py
+++ Python/000/071-Simplify-Path.py
@@ -25,6 +25,20 @@ class Solution:
                 stack.append(cur)
         return "/" + "/".join(stack)
 
+    def simplify_path_v3(self, path: str) -> str:
+        stack = []
+
+        for part in path.split("/"):
+            if part in ("", "."):
+                continue
+            if part == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(part)
+
+        return "/" + "/".join(stack)
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -48,3 +62,8 @@ class TestSolution(unittest.TestCase):
         for path, expected in self.test_cases:
             with self.subTest(path=path, expected=expected):
                 self.assertEqual(self.s.simplify_path_v2(path), expected)
+
+    def test_simplify_path_v3(self):
+        for path, expected in self.test_cases:
+            with self.subTest(path=path, expected=expected):
+                self.assertEqual(self.s.simplify_path_v3(path), expected)
