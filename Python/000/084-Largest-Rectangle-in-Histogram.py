10520c6c63331bb13c55e89ed40831700c9371f4
Atsushi <atu729@icloud.com>
2026-02-05 22:30:14 +0900

    refactor: convert largest_rectangle_area to a method in Solution class and update test cases

--- Python/000/084-Largest-Rectangle-in-Histogram.py
+++ Python/000/084-Largest-Rectangle-in-Histogram.py
@@ -2,34 +2,46 @@ import unittest
 
 
 #################### Solution ####################
-def largest_rectangle_area(heights: list[int]) -> int:
-    """
-    Time complexity: O(n)
-    Space complexity: O(n)
-    """
-    max_area = 0
-    stack = []
+class Solution:
+    def largest_rectangle_area(self, heights: list[int]) -> int:
+        """
+        Time complexity: O(n)
+        Space complexity: O(n)
+        """
+        max_area = 0
+        stack = []
 
-    for i, h in enumerate(heights):
-        start = i
+        for i, h in enumerate(heights):
+            start = i
 
-        while stack and stack[-1][1] > h:
-            index, height = stack.pop()
-            max_area = max(max_area, height * (i - index))
-            start = index
-        stack.append((start, h))
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                max_area = max(max_area, height * (i - index))
+                start = index
+            stack.append((start, h))
 
-    for i, h in stack:
-        max_area = max(max_area, h * (len(heights) - i))
+        for i, h in stack:
+            max_area = max(max_area, h * (len(heights) - i))
 
-    return max_area
+        return max_area
 
 
 #################### Test Case ####################
-class TestLargestRectangleArea(unittest.TestCase):
+class TestSolution(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.s = Solution()
+        cls.test_cases = [
+            ([2, 1, 5, 6, 2, 3], 10),
+            ([2, 4], 4),
+            ([6, 2, 5, 4, 5, 1, 6], 12),
+            ([1, 1], 2),
+            ([4, 2, 0, 3, 2, 5], 6),
+        ]
+
     def test_largest_rectangle_area(self):
-        self.assertEqual(largest_rectangle_area([2, 1, 5, 6, 2, 3]), 10)
-        self.assertEqual(largest_rectangle_area([2, 4]), 4)
-        self.assertEqual(largest_rectangle_area([2, 1, 2]), 3)
-        self.assertEqual(largest_rectangle_area([2, 1, 5, 6, 2, 3]), 10)
-        self.assertEqual(largest_rectangle_area([2, 1, 5, 6, 2, 3, 1, 2]), 10)
+        for heights, expected in self.test_cases:
+            self.assertEqual(
+                self.s.largest_rectangle_area(heights),
+                expected
+            )
