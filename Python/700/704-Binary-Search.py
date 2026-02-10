3a6dc79f7188e705eb585920be63bc8000407e81
Atsushi <atu729@icloud.com>
2026-02-10 09:31:32 +0900

    refactor: convert search function to method of Solution class and enhance test cases

--- Python/700/704-Binary-Search.py
+++ Python/700/704-Binary-Search.py
@@ -2,26 +2,38 @@ import unittest
 
 
 #################### Solution ####################
-def search(nums: list[int], target: int) -> int:
-    left, right = 0, len(nums) - 1
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
 
-    while left <= right:
-        mid = (left + right) // 2
+        while left <= right:
+            mid = (left + right) // 2
 
-        if nums[mid] == target:
-            return mid
+            if nums[mid] == target:
+                return mid
 
-        if nums[mid] <= target:
-            left = mid + 1
-        else:
-            right = mid - 1
+            if nums[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
 
-    return -1
+        return -1
 
 
 #################### Test Case ####################
 class TestSearch(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.s = Solution()
+        cls.test_cases = [
+            ([-1, 0, 3, 5, 9, 12], 9, 4),
+            ([-1, 0, 3, 5, 9, 12], 2, -1),
+            ([5], 5, 0),
+            ([1, 2, 3, 4, 5], 1, 0),
+            ([1, 2, 3, 4, 5], 5, 4),
+        ]
+
     def test_search(self):
-        nums = [-1, 0, 3, 5, 9, 12]
-        target = 9
-        self.assertEqual(search(nums, target), 4)
+        for nums, target, expected in self.test_cases:
+            with self.subTest(nums=nums, target=target, expected=expected):
+                self.assertEqual(self.s.search(nums, target), expected)
