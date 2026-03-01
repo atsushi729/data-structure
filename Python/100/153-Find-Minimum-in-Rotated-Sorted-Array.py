ee9d370b395a7015fa5b89db11f34471747ea4b8
Atsushi <atu729@icloud.com>
2026-03-01 10:34:07 +0900

    add: update test cases for find_min method in Solution class for improved coverage

--- Python/100/153-Find-Minimum-in-Rotated-Sorted-Array.py
+++ Python/100/153-Find-Minimum-in-Rotated-Sorted-Array.py
@@ -75,10 +75,11 @@ class TestFindMin(unittest.TestCase):
     def setUpClass(cls):
         cls.s = Solution()
         cls.test_cases = [
-            # ([3, 4, 5, 6, 1, 2], 1),
+            ([3, 4, 5, 6, 1, 2], 1),
             ([4, 1, 2, 3], 1),
             ([4, 5, 6, 7, 0, 1, 2], 0),
             ([11, 13, 15, 17], 11),
+            ([2, 1], 1),
         ]
 
     def test_find_min(self):
