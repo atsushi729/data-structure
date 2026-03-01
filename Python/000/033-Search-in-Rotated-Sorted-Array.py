d32f361dc171ce15f9f8507bdf2d17f80d2fc9a5
Atsushi <atu729@icloud.com>
2026-03-01 09:44:59 +0900

    add: refactor search function into Solution class with unit tests for improved structure

--- Python/000/033-Search-in-Rotated-Sorted-Array.py
+++ Python/000/033-Search-in-Rotated-Sorted-Array.py
@@ -2,37 +2,47 @@ import unittest
 
 
 #################### Solution ####################
-def search(nums: list[int], target: int) -> int:
-    """
-    Time complexity: O(log n)
-    Space complexity: O(1)
-    """
-    l, r = 0, len(nums) - 1
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        """
+        Time complexity: O(log n)
+        Space complexity: O(1)
+        """
+        l, r = 0, len(nums) - 1
 
-    while l <= r:
-        m = (l + r) // 2
+        while l <= r:
+            m = (l + r) // 2
 
-        if nums[m] == target:
-            return m
+            if nums[m] == target:
+                return m
 
-        if nums[l] <= nums[m]:
-            if target < nums[l] or target > nums[m]:
-                l = m + 1
+            if nums[l] <= nums[m]:
+                if target < nums[l] or target > nums[m]:
+                    l = m + 1
+                else:
+                    r = m - 1
+
             else:
-                r = m - 1
-
-        else:
-            if target > nums[r] or target < nums[m]:
-                r = m - 1
-            else:
-                l = m + 1
-    return -1
+                if target > nums[r] or target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+        return -1
 
 
 #################### Test Case ####################
 class TestSearch(unittest.TestCase):
-    def test_search(self):
-        self.assertEqual(search([4, 5, 6, 7, 0, 1, 2], 0), 4)
-        self.assertEqual(search([4, 5, 6, 7, 0, 1, 2], 3), -1)
-        self.assertEqual(search([1], 0), -1)
-        self.assertEqual(search([1], 1), 0)
+    @classmethod
+    def setUpClass(cls):
+        cls.s = Solution()
+        cls.test_cases = [
+            ([4, 5, 6, 7, 0, 1, 2], 0, 4),
+            ([4, 5, 6, 7, 0, 1, 2], 3, -1),
+            ([1], 0, -1),
+            ([1], 1, 0),
+        ]
+
+    def test_solution(self):
+        for nums, target, expected in self.test_cases:
+            with self.subTest(nums=nums, target=target):
+                self.assertEqual(self.s.search(nums, target), expected)
