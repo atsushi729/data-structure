8f797c37517833d06fa42eb8615269c7e09d948f
Atsushi <atu729@icloud.com>
2026-03-03 22:14:45 +0900

    add: refactor TimeMap class to use defaultdict for key-value storage and improve test structure

--- Python/900/981-Time-Based-Key-Value-Store.py
+++ Python/900/981-Time-Based-Key-Value-Store.py
@@ -1,24 +1,23 @@
 import unittest
+from collections import defaultdict
 
 
 #################### Solution ####################
 class TimeMap:
 
     def __init__(self):
-        self.key_store = {}  # key and list of [value, timestamp]
+        self.time_map = defaultdict(list)  # {key : [value, timestamp]}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if not key in self.key_store:
-            self.key_store[key] = []
-        self.key_store[key].append([value, timestamp])
+        self.time_map[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
-        res, values = "", self.key_store.get(key, [])
-        l, r = 0, len(values) - 1
+        res = ""
+        values = self.time_map.get(key, [])
 
+        l, r = 0, len(values) - 1
         while l <= r:
             m = (l + r) // 2
-
             if values[m][1] <= timestamp:
                 res = values[m][0]
                 l = m + 1
@@ -29,11 +28,15 @@ class TimeMap:
 
 #################### Test Case ####################
 class TestTimeMap(unittest.TestCase):
-    def test_time_map(self):
-        time_map = TimeMap()
-        time_map.set("foo", "bar", 1)
-        self.assertEqual(time_map.get("foo", 1), "bar")
-        self.assertEqual(time_map.get("foo", 3), "bar")
-        time_map.set("foo", "bar2", 4)
-        self.assertEqual(time_map.get("foo", 4), "bar2")
-        self.assertEqual(time_map.get("foo", 5), "bar2")
+    @classmethod
+    def setUpClass(cls):
+        cls.time_map = TimeMap()
+        cls.time_map.set("foo", "bar", 1)
+        cls.time_map.set("foo", "bar2", 4)
+
+    def test_get(self):
+        self.assertEqual(self.time_map.get("foo", 1), "bar")
+        self.assertEqual(self.time_map.get("foo", 3), "bar")
+        self.assertEqual(self.time_map.get("foo", 4), "bar2")
+        self.assertEqual(self.time_map.get("foo", 5), "bar2")
+        self.assertEqual(self.time_map.get("bar", 1), "")
