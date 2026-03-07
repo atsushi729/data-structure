c1060c3313687e2d116e796e410f076e3e4b2645
Atsushi <atu729@icloud.com>
2026-03-07 10:53:34 +0900

    add: enhance unit tests for find_median_sorted_arrays method in Solution class with setup for multiple test cases

--- Python/000/004-Median-of-Two-Sorted-Arrays.py
+++ Python/000/004-Median-of-Two-Sorted-Arrays.py
@@ -34,10 +34,18 @@ class Solution:
 
 #################### Test Case ####################
 class TestFindMedianSortedArrays(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.s = Solution()
+        cls.test_cases = [
+            ([1, 3], [2], 2.0),
+            ([1, 2], [3, 4], 2.5),
+            ([0, 0], [0, 0], 0.0),
+            ([], [1], 1.0),
+            ([2], [], 2.0)
+        ]
+
     def test_find_median_sorted_arrays(self):
-        solution = Solution()
-        self.assertEqual(solution.find_median_sorted_arrays([1, 3], [2]), 2.0)
-        self.assertEqual(solution.find_median_sorted_arrays([1, 2], [3, 4]), 2.5)
-        self.assertEqual(solution.find_median_sorted_arrays([0, 0], [0, 0]), 0.0)
-        self.assertEqual(solution.find_median_sorted_arrays([], [1]), 1.0)
-        self.assertEqual(solution.find_median_sorted_arrays([2], []), 2.0)
+        for nums1, nums2, expected in self.test_cases:
+            with self.subTest(nums1=nums1, nums2=nums2, expected=expected):
+                self.assertEqual(self.s.find_median_sorted_arrays(nums1, nums2), expected)
