220be85a427a06e637fc03a6608fc8a4fad6ed2c
Atsushi <atu729@icloud.com>
2026-03-24 19:38:16 +0900

    add: implement remove_nth_from_end_v2 method and enhance testing for both versions

--- Python/000/019-Remove-Nth-Node-From-End-of-List.py
+++ Python/000/019-Remove-Nth-Node-From-End-of-List.py
@@ -27,6 +27,21 @@ class Solution:
 
         return head
 
+    def remove_nth_from_end_v2(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        slow = fast = dummy
+
+        for _ in range(n):
+            fast = fast.next
+
+        while fast.next:
+            slow = slow.next
+            fast = fast.next
+
+        slow.next = slow.next.next
+
+        return dummy.next
+
 
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
@@ -50,8 +65,8 @@ class TestSolution(unittest.TestCase):
             head = head.next
         return result
 
-    def test_removeNthFromEnd(self):
-        test_cases = [
+    def get_test_cases(self):
+        return [
             {
                 "input": ([1, 2, 3, 4, 5], 2),
                 "expected": [1, 2, 3, 5],
@@ -64,16 +79,26 @@ class TestSolution(unittest.TestCase):
                 "input": ([1, 2], 1),
                 "expected": [1],
             },
+            {
+                "input": ([1, 2], 2),
+                "expected": [2],
+            },
         ]
 
-        for case in test_cases:
+    def test_remove_nth_from_end(self):
+        """ test code for remove_nth_from_end """
+        for case in self.get_test_cases():
             with self.subTest(case=case):
                 head = self.build_list(case["input"][0])
                 n = case["input"][1]
-
                 result = self.solution.remove_nth_from_end(head, n)
                 self.assertEqual(self.list_to_array(result), case["expected"])
 
-
-if __name__ == "__main__":
-    unittest.main()
+    def test_remove_nth_from_end_v2(self):
+        """ test code for remove_nth_from_end_v2 """
+        for case in self.get_test_cases():
+            with self.subTest(case=case):
+                head = self.build_list(case["input"][0])
+                n = case["input"][1]
+                result = self.solution.remove_nth_from_end_v2(head, n)
+                self.assertEqual(self.list_to_array(result), case["expected"])
