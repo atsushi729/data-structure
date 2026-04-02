1aa93c0c746297d472f7b174ad0988e2a84d6572
Atsushi <atu729@icloud.com>
2026-04-02 23:55:45 +0900

    add: implement MyCircularQueueV2 class with enqueue and dequeue methods and add corresponding unit tests

--- Python/600/622-Design-Circular-Queue.py
+++ Python/600/622-Design-Circular-Queue.py
@@ -47,9 +47,51 @@ class MyCircularQueue:
         return self.size == len(self.queue)
 
 
+class MyCircularQueueV2:
+    def __init__(self, k: int):
+        self.q = []
+        self.k = k
+
+    def enQueue(self, value: int) -> bool:
+        if len(self.q) == self.k:
+            return False
+        self.q.append(value)
+        return True
+
+    def deQueue(self) -> bool:
+        if not self.q:
+            return False
+        self.q.pop(0)
+        return True
+
+    def Front(self) -> int:
+        if self.q:
+            return self.q[0]
+        return -1
+
+    def Rear(self) -> int:
+        if self.q:
+            return self.q[-1]
+        return -1
+
+    def isEmpty(self) -> bool:
+        return len(self.q) == 0
+
+    def isFull(self) -> bool:
+        return len(self.q) == self.k
+
+
 class TestMyCircularQueue(unittest.TestCase):
     def test_my_circular_queue(self):
         circular_queue = MyCircularQueue(3)
+        self.assertEqual(circular_queue.enQueue(1), True)
+        self.assertEqual(circular_queue.enQueue(2), True)
+        self.assertEqual(circular_queue.enQueue(3), True)
+        self.assertEqual(circular_queue.enQueue(4), False)
+        self.assertEqual(circular_queue.Rear(), 3)
+
+    def test_my_circular_queue_v2(self):
+        circular_queue = MyCircularQueueV2(3)
         self.assertEqual(circular_queue.enQueue(1), True)
         self.assertEqual(circular_queue.enQueue(2), True)
         self.assertEqual(circular_queue.enQueue(3), True)
