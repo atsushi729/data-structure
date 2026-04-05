9a0816244b6abc163785bd081e842175adb9e1c9
Atsushi <atu729@icloud.com>
2026-04-05 17:16:38 +0900

    add: implement LRUCache class with get and put methods and add corresponding unit tests

--- Python/100/146-LRU-cache.py
+++ Python/100/146-LRU-cache.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import OrderedDict
 
 
 #################### Solution ####################
@@ -46,10 +47,43 @@ class LRUCacheWithLinkedList:
             del self.cache[least_node.key]
 
 
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.cap = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)
+
+
 #################### Test Case ####################
 class TestLRUCache(unittest.TestCase):
     def test_lru_cache(self):
         cache = LRUCacheWithLinkedList(2)
+        cache.put(1, 1)
+        cache.put(2, 2)
+        self.assertEqual(cache.get(1), 1)
+        cache.put(3, 3)
+        self.assertEqual(cache.get(2), -1)
+        cache.put(4, 4)
+        self.assertEqual(cache.get(1), -1)
+        self.assertEqual(cache.get(3), 3)
+        self.assertEqual(cache.get(4), 4)
+
+    def test_lru_cache_with_ordered_dict(self):
+        cache = LRUCache(2)
         cache.put(1, 1)
         cache.put(2, 2)
         self.assertEqual(cache.get(1), 1)
