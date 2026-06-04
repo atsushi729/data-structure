a5b1e806c61257150e0edf0d17464ed094da215d
Atsushi <atu729@icloud.com>
2026-06-04 22:01:06 +0900

    refactor: enhance test cases for KthLargest and KthLargestV2 with consolidated setup and subtests

--- Python/700/703-Kth-Largest-Element-in-a-Stream.py
+++ Python/700/703-Kth-Largest-Element-in-a-Stream.py
@@ -42,18 +42,52 @@ class KthLargestV2:
 
 #################### Test Case ####################
 class TestKthLargest(unittest.TestCase):
-    def test_add(self):
-        kth_largest = KthLargest(3, [4, 5, 8, 2])
-        self.assertEqual(kth_largest.add(3), 4)
-        self.assertEqual(kth_largest.add(5), 5)
-        self.assertEqual(kth_largest.add(10), 5)
-        self.assertEqual(kth_largest.add(9), 8)
-        self.assertEqual(kth_largest.add(4), 8)
 
-    def test_add_v2(self):
-        kth_largest = KthLargestV2(3, [4, 5, 8, 2])
-        self.assertEqual(kth_largest.add(3), 4)
-        self.assertEqual(kth_largest.add(5), 5)
-        self.assertEqual(kth_largest.add(10), 5)
-        self.assertEqual(kth_largest.add(9), 8)
-        self.assertEqual(kth_largest.add(4), 8)
+    @classmethod
+    def setUpClass(cls):
+        cls.test_cases = [
+            {
+                "k": 3,
+                "nums": [4, 5, 8, 2],
+                "operations": [3, 5, 10, 9, 4],
+                "expected": [4, 5, 5, 8, 8],
+            },
+            {
+                "k": 1,
+                "nums": [],
+                "operations": [-3, -2, -4, 0, 4],
+                "expected": [-3, -2, -2, 0, 4],
+            },
+            {
+                "k": 2,
+                "nums": [0],
+                "operations": [-1, 1, -2, 3],
+                "expected": [-1, 0, 0, 1],
+            },
+        ]
+
+        cls.implementations = [
+            KthLargest,
+            KthLargestV2,
+        ]
+
+    def test_kth_largest(self):
+        for implementation in self.implementations:
+            for case in self.test_cases:
+                with self.subTest(
+                        implementation=implementation.__name__,
+                        case=case,
+                ):
+                    kth_largest = implementation(
+                        case["k"],
+                        case["nums"].copy()
+                    )
+
+                    for value, expected in zip(
+                            case["operations"],
+                            case["expected"]
+                    ):
+                        self.assertEqual(
+                            kth_largest.add(value),
+                            expected
+                        )
