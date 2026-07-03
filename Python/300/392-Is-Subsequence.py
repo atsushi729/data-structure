7602e85d72a60b2e406d9bbb3ce3dd5cfbc260c2
Atsushi <atu729@icloud.com>
2026-07-03 17:34:54 +0900

    add: implement is_subsequence2 method and corresponding unit tests

--- Python/300/392-Is-Subsequence.py
+++ Python/300/392-Is-Subsequence.py
@@ -11,6 +11,21 @@ class Solution:
 
         return i == len(s)
 
+    def is_subsequence2(self, s: str, t: str) -> bool:
+        n, m = len(s), len(t)
+        dp = [[False] * (m + 1) for _ in range(n + 1)]
+
+        for j in range(m + 1):
+            dp[n][j] = True
+
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = dp[i][j + 1]
+        return dp[0][0]
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -25,3 +40,8 @@ class TestSolution(unittest.TestCase):
         for s, t, expected in self.test_cases:
             with self.subTest(s=s, t=t):
                 self.assertEqual(self.solution.is_subsequence(s, t), expected)
+
+    def test_is_subsequence2(self):
+        for s, t, expected in self.test_cases:
+            with self.subTest(s=s, t=t):
+                self.assertEqual(self.solution.is_subsequence2(s, t), expected)
