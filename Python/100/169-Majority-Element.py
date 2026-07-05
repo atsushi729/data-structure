e69c09d0bdfd5b76d54452ed08019a0cb791d1ce
Atsushi <atu729@icloud.com>
2026-07-06 00:49:04 +0900

    add: implement majority_element_2 method and corresponding unit tests

--- Python/100/169-Majority-Element.py
+++ Python/100/169-Majority-Element.py
@@ -12,6 +12,15 @@ class Solution:
                 return k
         return 0
 
+    def majority_element_2(self, nums: list[int]) -> int:
+        majority_threshold = len(nums) / 2
+        count_by_number = Counter(nums)
+
+        for number, count in count_by_number.items():
+            if count > majority_threshold:
+                return number
+        return 0
+
 
 class TestSolution(unittest.TestCase):
     @classmethod
@@ -23,6 +32,11 @@ class TestSolution(unittest.TestCase):
         ]
 
     def test_majority_element(self):
+        for nums, expected in self.test_cases:
+            with self.subTest(nums=nums):
+                self.assertEqual(self.solution.majority_element(nums), expected)
+
+    def test_majority_element_2(self):
         for nums, expected in self.test_cases:
             with self.subTest(nums=nums):
                 self.assertEqual(self.solution.majority_element(nums), expected)
