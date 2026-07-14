670977295c7e93fa5af499dcdf7a68538a79235e
Atsushi <atu729@icloud.com>
2026-07-14 21:28:23 +0900

    add: enhance unit tests for group_anagram functions and include group_anagram_v3

--- Python/000/049-Group-Anagram.py
+++ Python/000/049-Group-Anagram.py
@@ -5,35 +5,43 @@ import collections
 #################### Solution ####################
 def group_anagram(strs):
     anagrams = {}
+
     for s in strs:
-        key = ''.join(sorted(s))
+        key = "".join(sorted(s))
+
         if key in anagrams:
             anagrams[key].append(s)
         else:
             anagrams[key] = [s]
+
     return list(anagrams.values())
 
 
 def group_anagram_v2(strs: list[str]) -> list[list[str]]:
     anagrams = {}
+
     for s in strs:
         key = tuple(sorted(s))
+
         if key in anagrams:
             anagrams[key].append(s)
         else:
             anagrams[key] = [s]
+
     return list(anagrams.values())
 
 
-# Another solution
 def model_group_anagram(strs: list[str]) -> list[list[str]]:
     ans = collections.defaultdict(list)
 
     for s in strs:
         count = [0] * 26
+
         for c in s:
             count[ord(c) - ord("a")] += 1
+
         ans[tuple(count)].append(s)
+
     return list(ans.values())
 
 
@@ -49,22 +57,43 @@ def group_anagram_v3(strs: list[str]) -> list[list[str]]:
 
 #################### Test Case ####################
 class TestGroupAnagram(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.solutions = [
+            group_anagram,
+            group_anagram_v2,
+            model_group_anagram,
+            group_anagram_v3,
+        ]
+
+        cls.test_cases = [
+            (
+                ["eat", "tea", "tan", "ate", "nat", "bat"],
+                [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]],
+            ),
+            (
+                ["", ""],
+                [["", ""]],
+            ),
+            (
+                ["a"],
+                [["a"]],
+            ),
+            (
+                [],
+                [],
+            ),
+        ]
+
     def test_group_anagram(self):
-        self.assertEqual(
-            group_anagram(["eat", "tea", "tan", "ate", "nat", "bat"]), [['eat', 'tea', 'ate'], ['tan', 'nat'],
-                                                                        ['bat']])
+        for solution in self.solutions:
+            for input_strs, expected in self.test_cases:
+                with self.subTest(
+                        solution=solution.__name__,
+                        input=input_strs,
+                ):
+                    self.assertEqual(solution(input_strs), expected)
 
-    def test_model_group_anagram(self):
-        self.assertEqual(model_group_anagram(["eat", "tea", "tan", "ate", "nat", "bat"]), [['eat', 'tea', 'ate'],
-                                                                                           ['tan', 'nat'],
-                                                                                           ['bat']])
 
-    def test_group_anagram_v2(self):
-        self.assertEqual(
-            group_anagram_v2(["eat", "tea", "tan", "ate", "nat", "bat"]), [['eat', 'tea', 'ate'], ['tan', 'nat'],
-                                                                           ['bat']])
-
-    def test_group_anagram_v3(self):
-        self.assertEqual(
-            group_anagram_v3(["eat", "tea", "tan", "ate", "nat", "bat"]), [['eat', 'tea', 'ate'], ['tan', 'nat'],
-                                                                           ['bat']])
+if __name__ == "__main__":
+    unittest.main()
