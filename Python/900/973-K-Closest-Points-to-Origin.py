844d2fbfc054cd381e06c83a7ab6b87db84395d3
Atsushi <atu729@icloud.com>
2026-08-31 11:14:07 -0700

    add: enhance unit tests for k_closest methods with additional test cases

--- Python/900/973-K-Closest-Points-to-Origin.py
+++ Python/900/973-K-Closest-Points-to-Origin.py
@@ -21,17 +21,62 @@ class Solution:
 
         for _ in range(k):
             result.append(heapq.heappop(min_heap)[1])
+
         return result
 
 
 #################### Test Case ####################
 class TestSolution(unittest.TestCase):
-    def test_k_closest(self):
-        solution = Solution()
-        self.assertListEqual(solution.k_closest([[1, 3], [-2, 2]], 1), [[-2, 2]])
-        self.assertListEqual(solution.k_closest([[3, 3], [5, -1], [-2, 4]], 2), [[3, 3], [-2, 4]])
+    def setUp(self):
+        self.sol = Solution()
+        self.methods = [
+            self.sol.k_closest,
+            self.sol.k_closest_v2,
+        ]
 
-    def test_k_closest_v2(self):
-        solution = Solution()
-        self.assertListEqual(solution.k_closest_v2([[1, 3], [-2, 2]], 1), [[-2, 2]])
-        self.assertListEqual(solution.k_closest_v2([[3, 3], [5, -1], [-2, 4]], 2), [[3, 3], [-2, 4]])
+    def test_k_closest(self):
+        test_cases = [
+            {
+                "name": "k equals 1",
+                "points": [[1, 3], [-2, 2]],
+                "k": 1,
+                "expected": [[-2, 2]],
+            },
+            {
+                "name": "k equals 2",
+                "points": [[3, 3], [5, -1], [-2, 4]],
+                "k": 2,
+                "expected": [[3, 3], [-2, 4]],
+            },
+            {
+                "name": "single point",
+                "points": [[1, 1]],
+                "k": 1,
+                "expected": [[1, 1]],
+            },
+            {
+                "name": "all points",
+                "points": [[1, 0], [2, 0], [3, 0]],
+                "k": 3,
+                "expected": [[1, 0], [2, 0], [3, 0]],
+            },
+        ]
+
+        for method in self.methods:
+            for case in test_cases:
+                with self.subTest(
+                        method=method.__name__,
+                        case=case["name"],
+                ):
+                    points = [point[:] for point in case["points"]]
+
+                    result = method(points, case["k"])
+
+                    self.assertListEqual(
+                        result,
+                        case["expected"],
+                    )
+
+
+if __name__ == "__main__":
+    unittest.main()
